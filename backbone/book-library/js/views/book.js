86add6ac1a86d1514019da4206b6ea744118e4bd
Piotr Nowak <p.nowak2@gmail.com>
2015-09-04 17:55:03 +0200

    view tests started

---
+++ backbone/book-library/js/views/book.js
@@ -0,0 +1,5 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+	className: 'bookContainer'
+});
--- backbone/book-library/test/spec/views/bookSpec.js
+++ backbone/book-library/test/spec/views/bookSpec.js
@@ -1,3 +1,19 @@
 describe('Book View', function() {
-	
+
+	describe('Constructor', function() {
+		it('should exist', function() {
+			expect(app.BookView.prototype).toEqual(jasmine.any(Backbone.View));
+		});
+	});
+
+	describe('View Element', function() {
+		it('should be a div', function() {
+			expect(app.BookView.prototype.tagName).toBe('div');
+		});
+
+		it('should have class name', function() {
+			expect(app.BookView.prototype.className).toBe('bookContainer');
+		});
+	});
+
 });
