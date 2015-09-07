8dedc882e1b4afec165c4f7074f1b426a2a42fa3
Piotr Nowak <p.nowak2@gmail.com>
2015-09-07 14:07:03 +0200

    added some grunt plugins

--- backbone/book-library/Gruntfile.js
+++ backbone/book-library/Gruntfile.js
@@ -2,6 +2,17 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 	  pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: ['js/**/*.js'],
+			options: {
+				globals: {
+					_: false,
+					$: false
+				},
+				browser: true,
+				devel: true
+			}
+		},
 	  uglify: {
 	    options: {
 	      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -10,10 +21,22 @@ module.exports = function(grunt) {
 	      src: 'js/**/*.js',
 	      dest: 'build/<%= pkg.name %>.min.js'
 	    }
-	  }
+	  },
+		jasmine : {
+			src : 'js/**/*.js',
+			options : {
+				specs : 'test/**/*.js'
+			}
+		},
+		watch: {
+			files: '**/*.js',
+			tasks: ['jasmine']
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['uglify']);
 };
