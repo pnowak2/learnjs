2223ad98f0f22190994222491d543ddb64a85ada
Piotr Nowak <p.nowak2@gmail.com>
2015-09-07 14:20:36 +0200

    added jshint task

--- backbone/book-library/Gruntfile.js
+++ backbone/book-library/Gruntfile.js
@@ -34,6 +34,7 @@ module.exports = function(grunt) {
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
 	grunt.loadNpmTasks('grunt-contrib-watch');
