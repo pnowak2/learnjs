90ef1795e624f4cdcc4028d6842f1fdc3a2826cb
Piotr Nowak <p.nowak2@gmail.com>
2015-09-13 17:39:34 +0200

    promises

--- backbone/backbone-browserify/test/spec/models/todoViewSpec.js
+++ backbone/backbone-browserify/test/spec/models/todoViewSpec.js
@@ -8,4 +8,16 @@ describe('TodoView', function() {
 	it('should have render method', function() {
 		expect(TodoView.prototype.render).toBeDefined();
 	});
+
+	it('should have dom events defined', function() {
+		expect(TodoView.prototype.events).toBeDefined();
+	});
+
+	it('should have click event defined', function() {
+		expect(TodoView.prototype.events['click']).toBeDefined();
+	});
+
+	it('should have click event handled', function() {
+		expect(TodoView.prototype.events['click']).toEqual('clicked');
+	});
 });
---
+++ promises/test/spec/promiseSpec.js
@@ -0,0 +1,5 @@
+describe('Promise', function() {
+  it('should behave...', function() {
+    
+  });
+});
