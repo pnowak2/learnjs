d4c4f04edf34bccbc350089147cbc8fc3c4710ee
Piotr Nowak <p.nowak2@gmail.com>
2015-09-19 14:11:15 +0200

    constants in es6.

--- ES6/test/spec/models/sampleSpec.js
+++ ES6/test/spec/models/sampleSpec.js
@@ -1,8 +1,8 @@
 describe('block bindings', function() {
   describe('let declarations', function() {
-    it('let variable', function() {
+    it('let variable only accessible in scope', function() {
       if (true) {
-          let value = "blue";
+          let value = 'blue';
           expect(value).toBeDefined();
       }
 
@@ -11,15 +11,55 @@ describe('block bindings', function() {
       }
     });
 
-    it('var variable', function() {
+    it('var variable accessible also outside scope (hoisting)', function() {
       if (true) {
-          var value = "blue";
+          var value = 'blue';
           expect(value).toBeDefined();
       }
 
       if(true) {
-        expect(value).toBeDefined();
+        expect(value).toBe('blue');
       }
+    });
+
+    it('var in for loops is still accessible outside loop', function() {
+      for(var i = 0; i < 3; i++) {}
+      expect(typeof i).toBeDefined();
+    });
+
+    it('let in for loops is not accessible outside loop', function() {
+      for(let i = 0; i < 3; i++) {}
+      expect(typeof i).toEqual('undefined');
+    });
+  });
+
+  describe('constants', function() {
+    it('constant declaration', function() {
+      const maxItems = 30;
+      expect(maxItems).toBe(30);
+    });
+
+    it('cannot change constant after initialization', function() {
+      const maxItems = 30;
+      expect(maxItems).toBe(30);
+      // maxItems = 0 // is illegal
+    });
+
+    it('const available only in local scope', function() {
+      if (true) {
+          const maxItems = 30;
+          expect(maxItems).toBe(30);
+      }
+
+      if(true) {
+        expect(typeof maxItems).toEqual('undefined')
+      }
+    });
+
+    it('cannot redefine variable with the same name', function() {
+      var myVar = 5;
+      //const myVar = 20; // is illegal
+      expect(myVar).toBe(5);
     });
   });
 
