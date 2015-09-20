2339a2b386a96e8dc526a90984ecd20ddfc5244c
Piotr Nowak <p.nowak2@gmail.com>
2015-09-20 13:56:18 +0200

    tag functions.

--- ES6/test/spec/models/stringsAndRegularExpressionsSpec.js
+++ ES6/test/spec/models/stringsAndRegularExpressionsSpec.js
@@ -65,11 +65,64 @@ describe('unicode support', function() {
     	});
 
     	it('substitutions need to use backticks as string template', function() {
-    		var name = 'piotr nowak',
+    		let name = 'piotr nowak',
     				age = 35,
     				msg = `hello ${name}, doubled age is ${age * 2}.`;
 
     		expect(msg).toBe('hello piotr nowak, doubled age is 70.');
+    	});
+    });
+
+    describe('tagged templates - capture backtick string to function which formats the final output', function() {
+    	it('capture string literal and substitutions', function() {
+				var tagSpy = jasmine.createSpy(),
+						name = 'piotr',
+						number = 11,
+						msg,
+						calls,
+						firstArg,
+						restArgs;
+
+				msg = tagSpy`${name}, welcome to ${number} world.`
+
+	    	expect(tagSpy).toHaveBeenCalled();
+	    	calls = tagSpy.calls.mostRecent();
+	    	firstArg = calls.args[0];
+	    	restArgs = [calls.args[1], calls.args[2]];
+
+	    	expect(firstArg).toEqual(jasmine.any(Array));
+	    	expect(firstArg[0]).toBe('');
+	    	expect(firstArg[1]).toBe(', welcome to ');
+	    	expect(firstArg[2]).toBe(' world.');
+
+	    	expect(restArgs).toEqual(['piotr', 11]);
+    	});
+
+    	it('builds string with tag template', function() {
+    		let name = 'piotr',
+    				age = 35,
+    				msg,
+    				tag = function (literals, ...substitutions) {
+    					// var substitutions = Array.prototype.slice.call(arguments, 1); // same as ...substitutions
+    					var msg1 = literals[0] + substitutions[0],
+    							msg2 = literals[1] + substitutions[1];
+
+    					return `${msg2}, ${msg1}`;
+    				}
+
+    		msg = tag`Hello, my name is ${name} and I am ${age} old.`;
+
+    		expect(msg).toEqual(' and I am 35, Hello, my name is piotr');
+    	});
+
+    	it('raw values in literals.raw[i] - like decide to do with /n before its converted to new line', function() {
+    		var msg,
+    				tag = function(literals, ...substitutions) {
+    					return literals.raw[0].replace('\\n', '<br/>');
+    					// not possible with literals[0], because it already contains converted \n to new line instead of raw string
+    				};
+
+    		expect(tag`and below\nis that`).toEqual('and below<br/>is that');
     	});
     });
 });
