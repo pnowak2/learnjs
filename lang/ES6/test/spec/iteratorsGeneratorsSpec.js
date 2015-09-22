b413e6f1682051db17cc2bccb9965fe68d62363b
Piotr Nowak <p.nowak2@gmail.com>
2015-09-22 15:58:42 +0200

    iterators and generators

--- lang/ES6/test/spec/iteratorsGeneratorsSpec.js
+++ lang/ES6/test/spec/iteratorsGeneratorsSpec.js
@@ -1,5 +1,53 @@
 describe('Iterators', function() {
-	it('should behave...', function() {
-		
+
+	describe('classical ES5 iterator implementation', function() {
+		it('createIterator with next() method and done + value properties ', function() {
+			var createIterator = function (items) {
+				var i = 0;
+
+				return {
+					next() {
+						var done = (i >= items.length),
+								value = !done ? items[i++] : undefined;
+						return {value, done}
+					}
+				}
+			}
+
+			var it = createIterator([1, 2, 3]);
+
+			expect(it.next).toEqual(jasmine.any(Function));
+
+			expect(it.next()).toEqual({
+				done: false,
+				value: 1
+			});
+
+			expect(it.next()).toEqual({
+				done: false,
+				value: 2
+			});
+
+			expect(it.next()).toEqual({
+				done: false,
+				value: 3
+			});
+
+			expect(it.next()).toEqual({
+				done: true,
+				value: undefined
+			});
+
+			expect(it.next()).toEqual({
+				done: true,
+				value: undefined
+			});
+		});
+	});
+
+	describe('generators', function() {
+		it('should behave...', function() {
+			
+		});
 	});
 });
