5aa6bb2da9cff228467a51d3203a0bcad7bd9ee8
Piotr Nowak <p.nowak2@gmail.com>
2015-09-23 11:28:34 +0200

    next specs

--- lang/ES6/test/spec/7-iteratorsGeneratorsSpec.js
+++ lang/ES6/test/spec/7-iteratorsGeneratorsSpec.js
@@ -46,40 +46,112 @@ describe('Iterators', function() {
 	});
 
 	describe('generators (function which creates iterator object)', function() {
-		it('create generator with function * syntax', function() {
-			var createIterator = function *(items) {
-						for(let i = 0; i < items.length; i++) {
-							yield items[i];
-						}
-					}, 
-					it = createIterator([1, 2, 3]);
 
-			expect(it.next).toEqual(jasmine.any(Function));
+		describe('definition with function *', function() {
+			it('creates generator which returns iterator returning 1, 2, 3', function() {
+				// the execution stops on each yield returning it's value while next() is called.
+				var createIterator = function * () {
+							yield 1;
+							yield 2;
+							yield 3;
+						}, 
+						it = createIterator();
 
-			expect(it.next()).toEqual({
-				done: false,
-				value: 1
+				expect(it.next()).toEqual({
+					done: false,
+					value: 1
+				});
+
+				expect(it.next()).toEqual({
+					done: false,
+					value: 2
+				});
+
+				expect(it.next()).toEqual({
+					done: false,
+					value: 3
+				});
+
+				expect(it.next()).toEqual({
+					done: true,
+					value: undefined
+				});
 			});
 
-			expect(it.next()).toEqual({
-				done: false,
-				value: 2
-			});
+			it('create generator with function * syntax', function() {
+				var createIterator = function * (items) {
+							for(let i = 0; i < items.length; i++) {
+								yield items[i];
+							}
+						}, 
+						it = createIterator([1, 2, 3]);
 
-			expect(it.next()).toEqual({
-				done: false,
-				value: 3
-			});
+				expect(it.next).toEqual(jasmine.any(Function));
 
-			expect(it.next()).toEqual({
-				done: true,
-				value: undefined
-			});
+				expect(it.next()).toEqual({
+					done: false,
+					value: 1
+				});
 
-			expect(it.next()).toEqual({
-				done: true,
-				value: undefined
+				expect(it.next()).toEqual({
+					done: false,
+					value: 2
+				});
+
+				expect(it.next()).toEqual({
+					done: false,
+					value: 3
+				});
+
+				expect(it.next()).toEqual({
+					done: true,
+					value: undefined
+				});
+
+				expect(it.next()).toEqual({
+					done: true,
+					value: undefined
+				});
 			});
 		});
+
+		describe('Definition as object method', function() {
+			it('method notation in object', function() {
+				var o = {
+						*createIterator() {
+							yield 1;
+							yield 2;
+							yield 3;
+						}
+					},
+					it = o.createIterator();
+
+				for(let i of [1, 2, 3]) {
+					expect(it.next()).toEqual({
+						value: i,
+						done: false
+					});
+				}
+			});
+
+			it('method notation in class', function() {
+				class Person {
+						items: [1, 2, 3]
+
+						*[Symbol.iterator]() {
+							yield *this.items;
+						}
+				}
+
+				let piotr = new Person(),
+						j = 1;
+
+				for(let i of piotr) {
+					expect(i).toBe(j);
+					j++;
+				}
+			});
+		});
+
 	});
 });
--- lang/fundamantals/test/spec/ecma5.js
+++ lang/fundamantals/test/spec/ecma5.js
@@ -2,7 +2,7 @@ describe('ECMAScript 5', function() {
 	it('Object.defineProperty', function() {
 		var obj = {}
 
-		Object.defineProperty(obj, "prop", {
+		Object.defineProperty(obj, 'prop', {
 			value: 'my property',
 			writable: false,
 			configurable: false
@@ -17,6 +17,32 @@ describe('ECMAScript 5', function() {
 		delete obj.prop
 
 		expect(obj.prop).toBeDefined();
+	});
+
+	it('Object.defineProperty with get/set. Here the writable and value must not be specified', function() {
+		var obj = {},
+				setSpy = jasmine.createSpy('setter'),
+				getSpy = jasmine.createSpy('getter');
+
+		Object.defineProperty(obj, 'name', {
+			set: function (newValue) {
+				value = newValue
+				setSpy(newValue);
+			},
+
+			get: function () {
+				getSpy(value)
+				return value
+			}
+		});
+
+		expect(setSpy).not.toHaveBeenCalled();
+		obj.name = 'hello world';
+		expect(setSpy).toHaveBeenCalledWith('hello world');
+
+		expect(getSpy).not.toHaveBeenCalled();
+		expect(obj.name).toBe('hello world')
+		expect(getSpy).toHaveBeenCalledWith('hello world');
 	});
 
 	it('Object.defineProperties', function() {
@@ -108,7 +134,34 @@ describe('ECMAScript 5', function() {
 		expect(propNames).toEqual(['name', 'age']);
 	});
 
-	it('Object.keys()');
+	it('Object.keys() - gives all object OWN keys', function () {
+		var parentPrototype = {
+					age: 0
+				},
+				Person = function (name) {
+					this.name = name;
+				};
+
+		Person.prototype = Object.create(parentPrototype);
+
+		var peter = new Person('peter');
+
+		expect(Object.keys(peter)).toEqual(['name']);
+	});
+
+	it('Object.is() - checks identity of given objects', function () {
+		expect(Object.is(NaN, NaN)).toBe(true);
+		expect(Object.is('test', 'test')).toBe(true);
+		expect(Object.is(undefined, undefined)).toBe(true);
+		expect(Object.is(-0, -0)).toBe(true);
+		expect(Object.is(+0, +0)).toBe(true);
+
+		expect(Object.is({a: 'a'}, {a: 'a'})).toBe(false);
+
+		// the same does not work with ===
+		expect(NaN === NaN).toBe(false);
+	});
+
 	it('Object.freeze()');
 	it('Object.isFrozen()');
 	it('Object.seal()');
@@ -116,7 +169,6 @@ describe('ECMAScript 5', function() {
 	it('Object.getOwnPropertyDescriptor()');
 	it('Object.getPrototypeOf()');
 	it('Object.setPrototypeOf()');
-	it('Object.is()');
 	it('Object.isExtensible()');
 	it('Object.getOwnPropertyDescriptor()');
 	it('Object.getOwnPropertyDescriptor()');
