8994253512208d47b9d400c915a26c2ce1dfed5c
Piotr Nowak <p.nowak2@gmail.com>
2016-10-18 16:51:39 +0200

    User items component done.

--- frameworks/client/angular2/ngbook2/angular2_hello_world/src/app/app.module.ts
+++ frameworks/client/angular2/ngbook2/angular2_hello_world/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
