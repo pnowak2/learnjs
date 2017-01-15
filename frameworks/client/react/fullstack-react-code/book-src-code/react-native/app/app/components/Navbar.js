67b23dfc48783ce9e29fef86efb5fe71e539d5d5
Piotr Nowak <p.nowak2@gmail.com>
2017-01-15 19:56:12 +0100

    prepared starting ng-book2 and react full stack

---
+++ frameworks/client/react/fullstack-react-code/book-src-code/react-native/app/app/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react'
+import { View, Text } from 'react-native'
+import NavigationBar from 'react-native-navbar'
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  rightButton: PropTypes.func,
+  leftButton: PropTypes.func,
+}
+
+export default function Navbar (props) {
+  let optionalProps = {}
+  props.leftButton && (optionalProps.leftButton = props.leftButton())
+  props.rightButton && (optionalProps.rightButton = props.rightButton())
+  return (
+    <NavigationBar
+      {...optionalProps}
+      tintColor='#f7f7f7'
+      title={{title: props.title}}
+    />
+  )
+}
