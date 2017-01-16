f809ab12fb1b92bad48d86f0f5f3d4759421154e
Piotr Nowak <p.nowak2@gmail.com>
2017-01-16 16:20:58 +0100

    got to page 100 from ngbook2

--- frameworks/client/angular2/ngbook2/angular2-inventory-app/app.ts
+++ frameworks/client/angular2/ngbook2/angular2-inventory-app/app.ts
@@ -3,7 +3,7 @@ import {
   EventEmitter
 } from '@angular/core';
 
-import { NgModule } from "@angular/core";
+import { NgModule, Input, Output } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
@@ -24,22 +24,91 @@ class Product {
   selector: 'inventory-app',
   template: `
   <div class="inventory-app">
-    hello
+    <products-list
+      [productList]="products"
+      (onProductSelected)="productWasSelected($event)">
+    </products-list>
   </div>
   `
 })
 class InventoryApp {
+  products: Array<Product>;
 
+  constructor() {
+    this.products = [
+      new Product(
+        'ASSMALNG',
+        'Assmal Next Generation',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        129.99),
+      new Product(
+        'ASSMAL2',
+        'Assmal Current Generation',
+        '/resources/images/products/black-hat.jpg',
+        ['Woman', 'Shoes', 'Running Shoes'],
+        229.99),
+      new Product(
+        'ASSMAL One',
+        'Assmal Old Generation',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        329.99)
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ' + product);
+  }
+}
+
+@Component({
+  selector: 'products-list',
+  inputs: ['productList'],
+  outputs: ['onProductSelected'],
+  template: `
+    <div class="ui items">
+      <product-row
+        *ngFor="let myProduct of productList"
+        [product]="myProduct"
+        (click)="clicked(myProduct)"
+        [class.selected] = "isSelected(myProduct)">
+      </product-row>
+    </div>
+  `
+})
+class ProductsList {
+  productList: Array<Product>;
+  onProductSelected: EventEmitter<Product>;
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product) {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product) {
+    if(!product || !this.currentProduct) {
+      return false;
+    }
+
+    return product.sku === this.currentProduct.sku;
+  }
 }
 
 @NgModule({
-  declarations: [ 
-    InventoryApp
+  declarations: [
+    InventoryApp,
+    ProductsList
   ],
-  imports: [ BrowserModule ],
-  bootstrap: [ InventoryApp ]
+  imports: [BrowserModule],
+  bootstrap: [InventoryApp]
 })
-class InventoryAppModule {}
+class InventoryAppModule { }
 
 platformBrowserDynamic().bootstrapModule(InventoryAppModule);
 
