35e6efc708c309a4fb1ed9c7d41bad32dc458298
Piotr Nowak <p.nowak2@gmail.com>
2017-01-16 11:52:57 +0100

    started reddit angular2 project

--- frameworks/client/angular2/ngbook2/angular2-reddit/src/app/app.module.ts
+++ frameworks/client/angular2/ngbook2/angular2-reddit/src/app/app.module.ts
@@ -4,16 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent,
-    UserItemComponent,
-    UserListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
