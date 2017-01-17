860d464284cadee66796e245ded2005fa6767ec0
Piotr Nowak <p.nowak2@gmail.com>
2017-01-17 14:39:20 +0100

    started youtube search chapter.

--- frameworks/client/angular2/ngbook2/angular2-http/src/app/app.module.ts
+++ frameworks/client/angular2/ngbook2/angular2-http/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { YoutubeSearchComponent } from './youtube-search/youtube-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    YoutubeSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ frameworks/client/angular2/ngbook2/angular2-http/src/app/youtube-search/youtube-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-youtube-search',
+  templateUrl: './youtube-search.component.html',
+  styleUrls: ['./youtube-search.component.css']
+})
+export class YoutubeSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
