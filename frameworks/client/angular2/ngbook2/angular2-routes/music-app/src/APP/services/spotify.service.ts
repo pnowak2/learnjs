24aced52d10b6865c9d6d383038c493b5139765b
Piotr Nowak <p.nowak2@gmail.com>
2017-01-18 12:23:08 +0100

    finished router music app.

--- frameworks/client/angular2/ngbook2/angular2-routes/music-app/src/APP/services/spotify.service.ts
+++ frameworks/client/angular2/ngbook2/angular2-routes/music-app/src/APP/services/spotify.service.ts
@@ -1,20 +1,36 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
 @Injectable() export class SpotifyService {
+  static BASE_URL: string = 'https://api.spotify.com/v1';
+
   constructor(public http: Http) {
   }
 
-  searchByTrack(query: string) {
-    let params: string = [
+  query(URL: string, params?: Array<string>): Observable<any[]> {
+    let queryURL: string = `${SpotifyService.BASE_URL}${URL}`;
+
+    if (params) {
+      queryURL = `${queryURL}?${params.join('&')}`;
+    }
+
+    return this.http.request(queryURL).map((res: any) => res.json());
+  }
+
+  search(query: string, type: string): Observable<any[]> {
+    return this.query(`/search`, [
       `q=${query}`,
-      `type=track`
-    ].join('&');
+      `type=${type}`
+    ]);
+  }
 
-    let queryURL: string = `https://api.spotify.com/v1/search?${params}`;
+  searchByTrack(query: string) {
+    return this.search(query, 'track');
+  }
 
-    return this.http.request(queryURL).map(res => res.json());
-
+  getTrack(id: string): Observable<any[]> {
+    return this.query(`/tracks/${id}`);
   }
 }
--- frameworks/client/angular2/ngbook2/angular2-routes/music-app/src/APP/track/track.component.ts
+++ frameworks/client/angular2/ngbook2/angular2-routes/music-app/src/APP/track/track.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+  ActivatedRoute,
+} from '@angular/router';
+
+import { SpotifyService } from '../services/spotify.service';
 
 @Component({
   selector: 'app-track',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./track.component.css']
 })
 export class TrackComponent implements OnInit {
+  id: string;
+  track: Object;
 
-  constructor() { }
+  constructor(
+    private spotify: SpotifyService,
+    private router: Router,
+    private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    })
+  }
 
   ngOnInit() {
+    this.spotify
+      .getTrack(this.id)
+      .subscribe((res: any) => {
+        this.renderTrack(res);
+      });
+  }
+
+  renderTrack(res: any): void {
+    this.track = null;
+
+    if (res && res.album) {
+      this.track = res;
+    }
   }
 
 }
