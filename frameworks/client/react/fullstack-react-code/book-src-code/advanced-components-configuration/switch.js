dc31a66c93ca1df0a828a538618926edfb2e6f7c
Piotr Nowak <p.nowak2@gmail.com>
2017-01-19 21:02:35 +0100

    working on react switch.

--- frameworks/client/react/fullstack-react-code/book-src-code/advanced-components-configuration/switch.js
+++ frameworks/client/react/fullstack-react-code/book-src-code/advanced-components-configuration/switch.js
@@ -1,18 +1,49 @@
+const CREDITCARD = 'Creditcard';
+const BTC = 'Bitcoin';
+
 const Switch = React.createClass({
   getInitialState() {
     return {
-
+      payMethod: BTC
     }
+  },
+
+  select(method) {
+    return (evt) => {
+      this.setState({
+        payMethod: method
+      })
+    }
+  },
+
+  renderChoice(choice) {
+    let cssClasses = [];
+
+    if(this.state.payMethod === choice) {
+      cssClasses.push('active');
+    }
+
+    return (
+      <div className="choice"
+           className={cssClasses}
+           onClick={this.select(choice)}>
+        {choice}
+      </div>
+    );
   },
 
   render() {
     return (
-      <div></div>
+      <div className="switch">
+        {this.renderChoice(CREDITCARD)}
+        {this.renderChoice(BTC)}
+        Pay with: {this.state.payMethod}
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Component />,
+  <Switch />,
   document.getElementById('switch')
 );
