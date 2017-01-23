7f7638fab3448facceec2f24b443095a64a9005d
Piotr Nowak <p.nowak2@gmail.com>
2017-01-23 20:50:03 +0100

    done

--- frameworks/client/react/fullstack-react-code/book-src-code/advanced-components-configuration/children.js
+++ frameworks/client/react/fullstack-react-code/book-src-code/advanced-components-configuration/children.js
@@ -1,19 +1,34 @@
 const Parent = React.createClass({
   render() {
     return (
-      <Container><div>te</div></Container>
+      <Container component={<p />}>hello</Container>
     );
   }
 });
 
 const Container = React.createClass({
   propTypes: {
+    component: React.PropTypes.element.isRequired,
     children: React.PropTypes.element.isRequired
+  },
+
+  renderChild(childData, index) {
+    return React.createElement(
+      this.props.component,
+      {},
+      childData
+    );
   },
 
   render() {
     return (
-      <div><p>here is child</p><b>{this.props.children}</b></div>
+      <div>
+        <p>here is child</p>
+        {React.Children.map(
+          this.props.children,
+          this.renderChild)}
+        )
+      </div>
     );
   }
 });
