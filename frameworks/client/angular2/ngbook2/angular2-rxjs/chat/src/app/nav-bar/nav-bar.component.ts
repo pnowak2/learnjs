386348f07050119b633b48263b65660bfee46997
Piotr Nowak <p.nowak2@gmail.com>
2017-01-23 16:30:40 +0100

    finished chat chapters.

--- frameworks/client/angular2/ngbook2/angular2-rxjs/chat/src/app/nav-bar/nav-bar.component.ts
+++ frameworks/client/angular2/ngbook2/angular2-rxjs/chat/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Thread } from '../models/thread';
+import { Message } from '../models/message';
+import { User } from '../models/user';
+import { ThreadsService } from '../services/threads.service';
+import { MessagesService } from '../services/messages.service';
+import { UserService } from '../services/user.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'nav-bar',
@@ -6,10 +14,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  unreadMessagesCount: number;
 
-  constructor() { }
+  constructor(private messagesService: MessagesService,
+    private threadsService: ThreadsService) { }
 
   ngOnInit() {
+    this.messagesService.messages
+      .combineLatest(this.threadsService.currentThread, (messages: Message[], currentThread: Thread) => [currentThread, messages])
+      .subscribe(([currentThread, messages]: [Thread, Message[]]) => {
+        this.unreadMessagesCount = _.reduce(messages, (sum: number, m: Message) => {
+          let messageIsInCurrentThread: boolean = m.thread && currentThread && (currentThread.id === m.thread.id);
+          if(m && !m.isRead && !messageIsInCurrentThread) {
+            sum = sum + 1;
+          }
+
+          return sum;
+        }, 0)
+
+      })
   }
 
 }
