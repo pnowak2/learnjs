81576b02a02fe20c75c508339f7d3368c29fdf2e
Piotr Nowak <p.nowak2@gmail.com>
2017-01-31 09:17:09 +0100

    done

--- frameworks/client/angular2/ngbook2/angular2-redux/chat-redux/src/app/components/chat-message/chat-message.component.ts
+++ frameworks/client/angular2/ngbook2/angular2-redux/chat-redux/src/app/components/chat-message/chat-message.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../../models/message';
 
 @Component({
   selector: 'chat-message',
   templateUrl: './chat-message.component.html',
-  styleUrls: ['./chat-message.component.css']
+  styleUrls: ['./chat-message.component.css'],
+  inputs: ['message']
 })
 export class ChatMessageComponent implements OnInit {
+  message: Message;
 
   constructor() { }
 
