69a527bab2655aa242cdcd0c70122e2d61bb0b48
Piotr Nowak <p.nowak2@gmail.com>
2017-02-17 09:32:20 +0100

    services with promises.

--- frameworks/client/angular2/angular.io/e2-heroes-tutorial/src/app/app.component.ts
+++ frameworks/client/angular2/angular.io/e2-heroes-tutorial/src/app/app.component.ts
@@ -1,31 +1,31 @@
+import { HeroService } from './service/hero.service';
 import { Hero } from './model/hero';
-import { Component } from '@angular/core';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Heroes App';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  constructor(private heroesService: HeroService) {
+
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroesService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
-
 }
---
+++ frameworks/client/angular2/angular.io/e2-heroes-tutorial/src/app/service/hero.service.ts
@@ -0,0 +1,12 @@
+import { HEROES } from './../mock-heroes';
+import { Hero } from './../model/hero';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(HEROES), 2000);
+    });
+  }
+}
