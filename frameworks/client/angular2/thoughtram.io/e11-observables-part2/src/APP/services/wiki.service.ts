a13a70f9bd5ff052d030f1ae9969d3dd14ada5e4
Piotr Nowak <p.nowak2@gmail.com>
2017-02-23 09:44:24 +0100

    done with observables part 2.

---
+++ frameworks/client/angular2/thoughtram.io/e11-observables-part2/src/APP/services/wiki.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { URLSearchParams, Jsonp } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class WikiService {
+
+  constructor(private jsonp: Jsonp) { }
+
+  search(terms: Observable<string>, debounceDuration = 400) {
+    return terms
+      .debounceTime(debounceDuration)
+      .distinctUntilChanged()
+      .switchMap(term => this.rawsearch(term));
+  }
+
+  rawsearch(term: string): Observable<Array<string>> {
+    var search = new URLSearchParams();
+    search.set('action', 'opensearch');
+    search.set('search', term);
+    search.set('format', 'json');
+
+    return this.jsonp
+      .get('http://en.wikipedia.org/w/api.php?callback=JSONP_CALLBACK', { search })
+      .map(response => response.json()[1])
+  }
+}
