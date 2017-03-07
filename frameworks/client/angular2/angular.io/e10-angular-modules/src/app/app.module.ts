80a62047ec0713bd689e6717cf91770de299cdbe
Piotr Nowak <p.nowak2@gmail.com>
2017-03-07 09:30:30 +0100

    done

--- frameworks/client/angular2/angular.io/e10-angular-modules/src/app/app.module.ts
+++ frameworks/client/angular2/angular.io/e10-angular-modules/src/app/app.module.ts
@@ -7,19 +7,27 @@ import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './contact/contact.service';
+import { AwesomePipe } from './contact/awesome.pipe';
+import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HighlightDirective,
-    TitleComponent
+    TitleComponent,
+    ContactComponent,
+    ContactHighlightDirective,
+    AwesomePipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [UserService],
+  providers: [UserService, ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frameworks/client/angular2/angular.io/e10-angular-modules/src/app/contact/awesome.pipe.ts
@@ -0,0 +1,9 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'awesome' })
+
+export class AwesomePipe implements PipeTransform {
+  transform(phrase: string) {
+    return phrase ? 'Awesome ' + phrase : '';
+  }
+}
