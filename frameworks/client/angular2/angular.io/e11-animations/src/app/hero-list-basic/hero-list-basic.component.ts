5dc401f5faa2e90d420e32d056c7821e887ee2ca
Piotr Nowak <p.nowak2@gmail.com>
2017-03-08 11:28:15 +0100

    done

--- frameworks/client/angular2/angular.io/e11-animations/src/app/hero-list-basic/hero-list-basic.component.ts
+++ frameworks/client/angular2/angular.io/e11-animations/src/app/hero-list-basic/hero-list-basic.component.ts
@@ -13,7 +13,21 @@ import { Heroes } from '../services/hero.service';
 @Component({
   selector: 'hero-list-basic',
   templateUrl: './hero-list-basic.component.html',
-  styleUrls: ['./hero-list-basic.component.css']
+  styleUrls: ['./hero-list-basic.component.css'],
+  animations: [
+    trigger('heroState', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ]),
+  ]
 })
 export class HeroListBasicComponent {
   @Input() heroes: Heroes;
