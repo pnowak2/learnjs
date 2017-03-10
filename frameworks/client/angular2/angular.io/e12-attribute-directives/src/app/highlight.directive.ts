8f400bfd8511eba76ca22a32ce494c48d8d458a9
Piotr Nowak <p.nowak2@gmail.com>
2017-03-10 09:37:29 +0100

    done

---
+++ frameworks/client/angular2/angular.io/e12-attribute-directives/src/app/highlight.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[myHighlight]'
+})
+export class HighlightDirective {
+
+  @Input('myHighlight') highlightColor: string;
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onmouseenter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
