8e8f13afce7e71e60da90dbe7edbb4f2f33b75db
Piotr Nowak <p.nowak2@gmail.com>
2017-03-28 09:26:39 +0200

    done

--- frameworks/client/angular2/angular.io/e17-http-client/src/app/wiki-smart/wiki-smart.component.ts
+++ frameworks/client/angular2/angular.io/e17-http-client/src/app/wiki-smart/wiki-smart.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { WikipediaService } from '../services/wikipedia.service';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'wiki-smart',
-  templateUrl: './wiki-smart.component.html',
-  styleUrls: ['./wiki-smart.component.css']
+  selector: 'my-wiki-smart',
+  template: `
+    <h1>Smarter Wikipedia Demo</h1>
+    <p>Search when typing stops</p>
+    <input #term (keyup)="search(term.value)"/>
+    <ul>
+      <li *ngFor="let item of items | async">{{item}}</li>
+    </ul>`,
+  providers: [WikipediaService]
 })
 export class WikiSmartComponent implements OnInit {
+  items: Observable<string[]>;
 
-  constructor() { }
+  constructor(private wikipediaService: WikipediaService) { }
+
+  private searchTermStream = new Subject<string>();
+
+  search(term: string) { this.searchTermStream.next(term); }
 
   ngOnInit() {
+    this.items = this.searchTermStream
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap((term: string) => this.wikipediaService.search(term));
   }
-
 }
