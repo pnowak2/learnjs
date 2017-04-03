951fbf5080d70a144c973cf4756c14384f3085b3
Piotr Nowak <p.nowak2@gmail.com>
2017-04-03 13:42:33 +0200

    started with vuejs

---
+++ frameworks/client/vue/vuejs.org/my-project/src/components/Other.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="other">
+    {{ title }}
+    <button @click="changeTitle">Change</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        title: 'Welcome to other!'
+      }
+    },
+    methods: {
+      changeTitle () {
+        this.title = 'Boo !'
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .other {
+    background: grey;
+  }
+</style>
