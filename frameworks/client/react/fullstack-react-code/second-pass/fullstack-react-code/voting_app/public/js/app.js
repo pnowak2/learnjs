d32c0aa1fa8bb0e1908497a3bae0627c62846164
Piotr Nowak <p.nowak2@gmail.com>
2017-05-25 14:50:56 +0200

    making product data-driven

--- frameworks/client/react/fullstack-react-code/second-pass/fullstack-react-code/voting_app/public/js/app.js
+++ frameworks/client/react/fullstack-react-code/second-pass/fullstack-react-code/voting_app/public/js/app.js
@@ -1,1 +1,35 @@
+class Product extends React.Component {
+  render() {
+    return (
+      <div className='item'>
+        <div className="image">
+          <img src="images/products/image-aqua.png" alt=""/>
+        </div>
+        <div className="middle aligned content">
+          <div className="description">
+            <a>Fort Knight</a>
+            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img src="images/avatars/daniel.jpg" className="ui avatar image"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
+class ProductsList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        <Product />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ProductsList />, document.querySelector('#content'));
+
+// Making Product data-driven
