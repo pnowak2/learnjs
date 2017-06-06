1c8f388eca8e2eed48ca54785ac8a863c20668be
Piotr Nowak <p.nowak2@gmail.com>
2017-06-06 11:12:57 +0200

    finished simple state.

--- frameworks/client/react/fullstack-react-code/second-pass/fullstack-react-code/time_tracking_app/public/js/app.js
+++ frameworks/client/react/fullstack-react-code/second-pass/fullstack-react-code/time_tracking_app/public/js/app.js
@@ -82,6 +82,25 @@ class EditableTimer extends React.Component {
 }
 
 class TimerForm extends React.Component {
+  state = {
+    title: this.props.title | '',
+    project: this.props.project | ''
+  }
+
+  handleTitleChange = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+
+    console.log(e.target.value);
+  }
+
+  handleProjectChange = (e) => {
+    this.setState({
+      project: e.target.value
+    })
+  }
+
   render() {
     const submitText = this.props.title ? 'Update' : 'Create';
 
@@ -91,11 +110,19 @@ class TimerForm extends React.Component {
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} />
+              <input 
+                type='text' 
+                value={this.state.title} 
+                onChange={this.handleTitleChange}
+              />
             </div>
             <div className='field'>
               <label>Project</label>
-              <input type='text' defaultValue={this.props.project} />
+              <input 
+                type='text' 
+                value={this.state.project} 
+                onChange={this.handleProjectChange}
+              />
             </div>
             <div className='ui two bottom attached buttons'>
               <button className='ui basic blue button'>
