d4e619d6e9dc466cb3686ff49b98a77396564493
Piotr Nowak <p.nowak2@gmail.com>
2017-09-04 08:40:10 +0200

    user list app done

--- frameworks/client/angular4/ngbook/1-first-angular-app/angular-hello-world/src/app/app.module.ts
+++ frameworks/client/angular4/ngbook/1-first-angular-app/angular-hello-world/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
