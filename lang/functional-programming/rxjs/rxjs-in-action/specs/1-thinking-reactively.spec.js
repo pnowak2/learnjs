0657adf207c64d6c99fe56994566afa0561fce33
Piotr Nowak <p.nowak2@gmail.com>
2017-09-04 13:30:25 +0200

    started rxjs in action.

---
+++ lang/functional-programming/rxjs/rxjs-in-action/specs/1-thinking-reactively.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import EventEmitter from 'events'
+
+describe('1 Thinking Reactively', () => {
+  describe('1.1 Synchronous versus asynchronous computing', () => {
+    describe('1.1.5 Event Emitters', () => {
+      it('should use node EventEmitter', (done) => {
+
+      });
+    });
+  });
+});
