06dba429714e566c83a9ef357aac294d9658fad9
Piotr Nowak <p.nowak2@gmail.com>
2018-03-05 16:28:19 +0100

    done

--- frameworks/client/angular5/udemy/18-http/http-start/src/app/app.component.ts
+++ frameworks/client/angular5/udemy/18-http/http-start/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServerService } from './server.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appName = this.serverService.getAppName();
+
   servers = [
     {
       name: 'Testserver',
@@ -18,12 +22,35 @@ export class AppComponent {
       id: this.generateId()
     }
   ];
+
+  constructor(private serverService: ServerService) { }
+
   onAddServer(name: string) {
     this.servers.push({
       name: name,
       capacity: 50,
       id: this.generateId()
     });
+  }
+  onSave() {
+    this.serverService.storeServers(this.servers).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  onGet() {
+    this.serverService.getServers().subscribe(
+      (servers: any[]) => {
+        this.servers = servers;
+      },
+      (error) => {
+        console.log('oops, ' + error);
+      }
+    );
   }
   private generateId() {
     return Math.round(Math.random() * 10000);
