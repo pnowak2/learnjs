ee06820aea5b55b3094f981b7d5e1d59c4720512
Piotr Nowak <p.nowak2@gmail.com>
2018-03-06 08:08:08 +0100

    finished http example.

--- frameworks/client/angular5/udemy/18-http/prj-forms-final/src/app/header/header.component.ts
+++ frameworks/client/angular5/udemy/18-http/prj-forms-final/src/app/header/header.component.ts
@@ -1,3 +1,5 @@
+import { Response } from '@angular/http';
+import { DataStorageService } from './../shared/data-storage.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) { }
+
+  onSaveData() {
+    this.dataStorageService
+      .storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 }
