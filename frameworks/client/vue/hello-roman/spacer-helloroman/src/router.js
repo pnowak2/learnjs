d796be1ad81fc51cbae139b44240836f1c54dee9
Piotr Nowak <p.nowak2@gmail.com>
2018-07-18 13:02:50 +0200

    connect the nasa api

--- frameworks/client/vue/hello-roman/spacer-helloroman/src/router.js
+++ frameworks/client/vue/hello-roman/spacer-helloroman/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
@@ -9,12 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Home',
+      component: Search,
     },
     {
       path: '/about',
-      name: 'about',
+      name: 'About',
       component: About,
     },
   ],
