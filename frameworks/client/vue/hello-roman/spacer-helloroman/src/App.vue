b3e8fb1c5fcc0050de74eb2d23fd12ce0d597a63
Piotr Nowak <p.nowak2@gmail.com>
2018-07-23 16:23:41 +0200

    done

--- frameworks/client/vue/hello-roman/spacer-helloroman/src/App.vue
+++ frameworks/client/vue/hello-roman/spacer-helloroman/src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="wrapper">
-    <HeroImage v-if="step === 0"/>
+  <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+    <transition name="slide">
+      <img src="./assets/logo.svg" alt="logo" class="logo" v-if="step === 1">
+    </transition>
+    <transition name="fade">
+      <HeroImage v-if="step === 0"/>
+    </transition>
     <Claim v-if="step === 0"/>
     <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item v-for="item in results" :key="item.data[0].nasa_id" :item="item"/>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 import axios from 'axios';
 import { debounce } from 'lodash';
 
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
@@ -24,6 +31,7 @@ export default {
     Claim,
     SearchInput,
     HeroImage,
+    Item,
   },
   data() {
     return {
@@ -73,5 +81,31 @@ body {
   margin: 0;
   padding: 30px;
   min-height: 100vh;
+  position: relative;
+
+  &.flexStart {
+    justify-content: flex-start;
+  }
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: margin-top 0.3s ease;
+}
+
+.slide-enter, .slide-leave-to {
+  margin-top: 50px;
+}
+
+.logo {
+  position: absolute;
+  top: 30px;
 }
 </style>
---
+++ frameworks/client/vue/hello-roman/spacer-helloroman/src/components/Item.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :style="style" class="item"></div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      title: this.item.data[0].title,
+      photo: this.item.links[0].href,
+    };
+  },
+  computed: {
+    style() {
+      return `background-image: url(${this.photo})`;
+    },
+  },
+  // mounted() {
+  //   this.title = this.item.data[0].title;
+  //   this.photo = this.item.links[0].href;
+  // },
+};
+</script>
+
+<style lang="scss" scoped>
+.item {
+  width: 300px;
+  height: 300px;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
+
