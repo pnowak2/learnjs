e6b7e10e2add8f61a01f93f4352a38ed9a61a305
Piotr Nowak <p.nowak2@gmail.com>
2019-01-28 10:27:07 +0100

    events streams with timeout/ timeinterval

--- frameworks/client/rxjs/angular-university/rxjs-course-introduction/1-operators-finished/src/app/about/about.component.ts
+++ frameworks/client/rxjs/angular-university/rxjs-course-introduction/1-operators-finished/src/app/about/about.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge } from 'rxjs';
+import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -12,11 +12,21 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+        let counter = 0;
 
+        document.addEventListener('click', evt => {
+            console.log(evt);
 
+            const handlerOut = setTimeout(() => {
+                console.log('finished.');
+
+                const handler = setInterval(() => {
+                    console.log(counter);
+                    counter++;
+                }, 1000);
+            }, 3000);
+        });
     }
-
-
 }
 
 
