d000bd9a1bdce0fed1e93e462807a41cc02be9ac
Piotr Nowak <p.nowak2@gmail.com>
2019-01-28 09:24:03 +0100

    starting rxjs course from angular university

---
+++ frameworks/client/rxjs/angular-university/rxjs-course-introduction/1-operators-finished/src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,82 @@
+import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {Course} from "../model/course";
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import * as moment from 'moment';
+import {fromEvent} from 'rxjs';
+import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/internal-compatibility';
+
+@Component({
+    selector: 'course-dialog',
+    templateUrl: './course-dialog.component.html',
+    styleUrls: ['./course-dialog.component.css']
+})
+export class CourseDialogComponent implements OnInit, AfterViewInit {
+
+    form: FormGroup;
+
+    course:Course;
+
+    @ViewChild('saveButton') saveButton: ElementRef;
+
+    @ViewChild('searchInput') searchInput : ElementRef;
+
+    constructor(
+        private fb: FormBuilder,
+        private dialogRef: MatDialogRef<CourseDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) course:Course ) {
+
+        this.course = course;
+
+        this.form = fb.group({
+            description: [course.description, Validators.required],
+            category: [course.category, Validators.required],
+            releasedAt: [moment(), Validators.required],
+            longDescription: [course.longDescription,Validators.required]
+        });
+
+    }
+
+    ngOnInit() {
+
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid),
+                concatMap(changes => this.saveCourse(changes))
+            )
+            .subscribe();
+
+
+    }
+
+
+    saveCourse(changes) {
+        return fromPromise(fetch(`/api/courses/${this.course.id}`,{
+            method: 'PUT',
+            body: JSON.stringify(changes),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }));
+    }
+
+
+    ngAfterViewInit() {
+
+        fromEvent(this.saveButton.nativeElement, 'click')
+            .pipe(
+                exhaustMap(() => this.saveCourse(this.form.value))
+            )
+            .subscribe();
+
+    }
+
+
+
+    close() {
+        this.dialogRef.close();
+    }
+
+
+}
---
+++ frameworks/client/rxjs/angular-university/rxjs-course-introduction/1-operators-finished/src/app/course/course.component.ts
@@ -0,0 +1,91 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Course} from "../model/course";
+import {
+    debounceTime,
+    distinctUntilChanged,
+    startWith,
+    tap,
+    delay,
+    map,
+    concatMap,
+    switchMap,
+    withLatestFrom,
+    concatAll, shareReplay
+} from 'rxjs/operators';
+import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {Lesson} from '../model/lesson';
+import {createHttpObservable} from '../common/util';
+import {debug, RxJsLoggingLevel, setRxJsLoggingLevel} from '../common/debug';
+
+
+@Component({
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit, AfterViewInit {
+
+    courseId:string;
+
+    course$ : Observable<Course>;
+
+    lessons$: Observable<Lesson[]>;
+
+
+    @ViewChild('searchInput') input: ElementRef;
+
+    constructor(private route: ActivatedRoute) {
+
+
+    }
+
+    ngOnInit() {
+
+        this.courseId = this.route.snapshot.params['id'];
+
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+            .pipe(
+                debug( RxJsLoggingLevel.INFO, "course value "),
+            );
+
+        setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+
+    }
+
+    ngAfterViewInit() {
+
+        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+            .pipe(
+                map(event => event.target.value),
+                startWith(''),
+                debug( RxJsLoggingLevel.TRACE, "search "),
+                debounceTime(400),
+                distinctUntilChanged(),
+                switchMap(search => this.loadLessons(search)),
+                debug( RxJsLoggingLevel.DEBUG, "lessons value ")
+            );
+
+    }
+
+    loadLessons(search = ''): Observable<Lesson[]> {
+        return createHttpObservable(
+            `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
+            .pipe(
+                map(res => res["payload"])
+            );
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
