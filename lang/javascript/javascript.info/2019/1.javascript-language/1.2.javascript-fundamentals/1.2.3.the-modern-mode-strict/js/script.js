f81993db5ffbf9092d5f19d5a71eca9bbc0b30e0
Piotr Nowak <p.nowak2@gmail.com>
2019-03-11 13:43:51 +0100

    done

---
+++ lang/javascript/javascript.info/2019/1.javascript-language/1.2.javascript-fundamentals/1.2.3.the-modern-mode-strict/js/script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// not allowed in strict mode
+c = 2;
+console.log(c);
--- lang/javascript/javascript.info/2019/specs/1.javascript-language/2.javascript-fundamentals.spec.js
+++ lang/javascript/javascript.info/2019/specs/1.javascript-language/2.javascript-fundamentals.spec.js
@@ -8,6 +8,20 @@ describe('2. JavaScript Fundamentals', () => {
   });
 
   describe('2.3 Modern mode, "use strict"', () => {
-    xit('should read the section', () => { });
+    it('should read the section', () => { });
+  });
+
+  describe('2.4 Variables', () => {
+    it('should declare variable', () => {
+      let message = 'msg';
+      expect(message).toEqual('msg');
+    });
+
+    it('should declare variables in same line', () => {
+      let m1 = 1, m2 = 2;
+
+      expect(m1).toEqual(1);
+      expect(m2).toEqual(2);
+    });
   });
 });
