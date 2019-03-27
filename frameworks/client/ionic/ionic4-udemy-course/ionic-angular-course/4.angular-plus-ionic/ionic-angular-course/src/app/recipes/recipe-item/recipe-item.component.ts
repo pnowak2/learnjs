6c1194e54fff05415c6ed998c5fda36dc3e9769c
Piotr Nowak <p.nowak2@gmail.com>
2019-03-27 13:16:43 +0100

    done

---
+++ frameworks/client/ionic/ionic4-udemy-course/ionic-angular-course/4.angular-plus-ionic/ionic-angular-course/src/app/recipes/recipe-item/recipe-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.scss'],
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
