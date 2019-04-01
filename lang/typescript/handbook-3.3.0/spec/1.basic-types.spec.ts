e520b383ba93145785342d17fffdd058e4cf48e9
Piotr Nowak <p.nowak2@gmail.com>
2019-04-01 16:36:29 +0200

    done

--- lang/typescript/handbook-3.3.0/spec/1.basic-types.spec.ts
+++ lang/typescript/handbook-3.3.0/spec/1.basic-types.spec.ts
@@ -1,9 +1,35 @@
 describe('Basic Types', () => {
   describe('Boolean', () => {
     it('should declare it', () => {
-      let isDone: boolean = false;
+      function POST(value: string) {
+        return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+          var originalMethod = descriptor.value;
+            descriptor.value = function() {
+              const res = originalMethod.apply(undefined);
+              return `http call to: ${value}, ${res}`;
+            }
 
-      expect(isDone).toBe(false);
+            return descriptor;
+        };
+    }
+
+      function f() {
+        return function(target: any) {
+          target.boo = 'hej';
+        }
+      }
+
+      @f()
+      class Person {
+        @POST('/api')
+        getAll() {
+          return 'orig';
+        }
+      }
+
+      const p = new Person();
+      expect(p.getAll()).toEqual('http call to: /api, orig');
+
     });
   })
 });
