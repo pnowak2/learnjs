c47f12e29605bb023472f878170d2791f1f6b3d1
Piotr Nowak <p.nowak2@gmail.com>
2019-04-03 21:31:13 +0200

    done

--- lang/javascript/javascript.info/2019/specs/1.javascript-language/6.advanced-working-with-functions.spec.js
+++ lang/javascript/javascript.info/2019/specs/1.javascript-language/6.advanced-working-with-functions.spec.js
@@ -1,7 +1,5 @@
 describe('6 Advanced working with functions', () => {
   describe('6.1 Recursion and Stack', () => {
-    it('should behave...', () => {
-      
-    });
+    it('should read the section', () => { });
   });
 });
