16cd109db9ac6bd5de4c0a13b136d9341fa5839a
Piotr Nowak <p.nowak2@gmail.com>
2019-04-10 20:47:55 +0200

    done

--- lang/javascript/javascript.info/2019/specs/1.javascript-language/12.generators-advanced-iteration.spec.js
+++ lang/javascript/javascript.info/2019/specs/1.javascript-language/12.generators-advanced-iteration.spec.js
@@ -1,7 +1,37 @@
 describe('12 Generators, advanced iteration', () => {
   describe('12.1 Generators', () => {
-    it('should behave...', () => {
-      
+    it('should use star syntax', () => {
+      function* gen() {
+        yield 1;
+        yield 2;
+        return 3;
+      }
+
+      const g = gen();
+
+      expect(g.next()).toEqual({ done: false, value: 1 });
+      expect(g.next()).toEqual({ done: false, value: 2 });
+      expect(g.next()).toEqual({ done: true, value: 3 });
+      expect(g.next()).toEqual({ done: true, value: undefined });
+    });
+
+    it('should be iterable', () => {
+      function* gen() {
+        yield 1;
+        yield 2;
+        yield 3;
+      }
+
+      const g1 = gen();
+      const g2 = gen();
+
+      let result = '';
+      for(let v of g1) {
+        result += v;
+      }
+
+      expect(result).toEqual('123');
+      expect([...g2]).toEqual([1, 2, 3]);
     });
   });
 });
