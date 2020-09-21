827a905fb763f7a02e13770be8fc4c75001de142
Piotr Nowak <p.nowak2@gmail.com>
2020-09-21 09:33:38 +0200

    added pipe() operator

--- frameworks/client/rxjs/build-own-rxjs/src/rxjs/observable.ts
+++ frameworks/client/rxjs/build-own-rxjs/src/rxjs/observable.ts
@@ -1,8 +1,22 @@
 import { Observer } from './observer';
 import { Subscription } from './subscription';
 
+type OperatorFunction<A, B> = (source: Observable<A>) => Observable<B>;
 export class Observable<T> {
     constructor(private observe: (observer: Observer<T>) => (() => void) | void) { }
+
+    pipe(): Observable<T>;
+    pipe<A>(oper1: OperatorFunction<T, A>): Observable<A>;
+
+    pipe(...pipeline: Array<OperatorFunction<any, any>>): Observable<any> {
+        let result: Observable<any> = this;
+
+        pipeline.forEach(operator => {
+            result = operator(result)
+        });
+
+        return result;
+    }
 
     subscribe(
         next?: (value: T) => void,
