cbb794a444f41b2e54ccb73665012facf61bb9ca
Piotr Nowak <p.nowak2@gmail.com>
2020-11-05 18:32:03 +0100

    first docker file done

--- tools/docker/docker-kubernetes-udemy/section-1/first-demo-starting-setup/app.mjs
+++ tools/docker/docker-kubernetes-udemy/section-1/first-demo-starting-setup/app.mjs
@@ -5,7 +5,7 @@ import connectToDatabase from './helpers.mjs'
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h2>Hi there!</h2>');
+    res.send('<h2>Hi there2!</h2>');
 });
 
 await connectToDatabase();
