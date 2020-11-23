14bca1d36826c424c6de3250b30795330f4e2798
Piotr Nowak <p.nowak2@gmail.com>
2020-11-23 15:41:21 +0100

    init webcomponents project

---
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 3/tooltip.js
@@ -0,0 +1,40 @@
+class Tooltip extends HTMLElement {
+    constructor() {
+        super();
+        this._tooltipContainer;
+        this._tooltipText = 'Some dummy tooltip text.';
+    }
+
+    connectedCallback() {
+        if(this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+
+        const tooltipIcon = document.createElement('span');
+        tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+
+        this.appendChild(tooltipIcon);
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+       this.removeChild(this._tooltipContainer);
+    }
+
+    disconnectedCallback() {
+        console.log('disconnected');
+    }
+
+    attributeChangedCallback() {
+        console.log('attribute changed');
+    }
+}
+
+customElements.define('uc-tooltip', Tooltip);
