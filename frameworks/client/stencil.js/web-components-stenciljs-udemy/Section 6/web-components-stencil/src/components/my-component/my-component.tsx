af4ec9475e14f84051eff464e1716adcd3da3afe
Piotr Nowak <p.nowak2@gmail.com>
2020-11-25 11:43:36 +0100

    stencil component

---
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 6/web-components-stencil/src/components/my-component/my-component.tsx
@@ -0,0 +1,21 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  @Prop() first: string;
+  @Prop() middle: string;
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <div>Hello, World! I'm {this.getText()}</div>;
+  }
+}
