92010a24f34ea37e3130236dda9213ca3fc6457d
Piotr Nowak <p.nowak2@gmail.com>
2020-11-26 20:25:26 +0100

    embedded webcomponent on node via publish npm package with webcomp

--- frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/node/app.js
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/node/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static('node_modules'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
  
