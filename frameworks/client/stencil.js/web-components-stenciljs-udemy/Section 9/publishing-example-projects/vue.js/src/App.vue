d462bd80d714040acc9be31ce39007bf5b00a8d7
Piotr Nowak <p.nowak2@gmail.com>
2020-11-26 21:07:54 +0100

    embedded web component in vue app

--- frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/vue.js/src/App.vue
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/vue.js/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <uc-stock-price stock-symbol="AAPL"></uc-stock-price>
+    <uc-stock-finder></uc-stock-finder>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --color-primary: #3b013b;
+  --color-primary-inverse: white;
+  --color-primary-highlight: #750175;
 }
 </style>
--- frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/vue.js/src/main.js
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/vue.js/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { defineCustomElements } from 'domisoft-my-web-component';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+defineCustomElements(window);
