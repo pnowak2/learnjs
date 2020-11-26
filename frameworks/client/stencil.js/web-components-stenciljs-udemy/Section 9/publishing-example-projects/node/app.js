c63b6ca22191f2ee167259b2da417872dab44984
Piotr Nowak <p.nowak2@gmail.com>
2020-11-26 20:34:01 +0100

    fix typos

--- frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/node/app.js
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/Section 9/publishing-example-projects/node/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
