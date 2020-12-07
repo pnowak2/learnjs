79b384412e143bb5a492fe6fe36eec9beaad3943
Piotr Nowak <p.nowak2@gmail.com>
2020-12-07 20:15:01 +0100

    done

--- frameworks/client/ionic/ionic-udemy-course/Chapter 7/booking/src/app/app-routing.module.ts
+++ frameworks/client/ionic/ionic-udemy-course/Chapter 7/booking/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './auth/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'places/discover',
+    redirectTo: 'places/tabs/discover',
     pathMatch: 'full'
   },
   {
