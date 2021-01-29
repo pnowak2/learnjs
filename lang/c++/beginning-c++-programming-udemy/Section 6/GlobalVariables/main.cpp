a2fdca405659ba51346b706a16a5426d9cbf30bc
Piotr Nowak <p.nowak2@gmail.com>
2021-01-29 16:50:18 +0100

    done

---
+++ lang/c++/beginning-c++-programming-udemy/Section 6/GlobalVariables/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int age {18};
+    
+int main() {
+
+    int age {2};
+    
+    cout << age << endl;
+    
+    return 0;
+}
---
+++ lang/c++/beginning-c++-programming-udemy/Section 6/SizeOfOperator/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <climits>
+#include <cfloat>
+
+#define pi 3.14;
+
+int main() {
+    std::cout << pi << std::endl;
+    const float f = 5.1F;
+    return 0;
+}
