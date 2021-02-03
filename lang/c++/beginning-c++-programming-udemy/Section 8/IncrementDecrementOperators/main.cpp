ebff9818f18ed7119bbce0eb6553fc9e8e43a25c
Piotr Nowak <p.nowak2@gmail.com>
2021-02-03 15:05:45 +0100

    done

---
+++ lang/c++/beginning-c++-programming-udemy/Section 8/IncrementDecrementOperators/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int counter {10};
+    int result {0};
+    
+    
+    
+    return 0;
+}
---
+++ lang/python/learn-python-programming-masterclass/ProgramFlow/aa.py
@@ -0,0 +1,11 @@
+number = 5
+multiplier = 8
+answer = 0
+
+# add your loop after this comment
+
+for i in range(5):
+    answer += multiplier
+
+print(answer)
+
