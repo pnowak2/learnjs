bbec8a785fe1888e37fab9b02c3e54e453faa6cf
Piotr Nowak <p.nowak2@gmail.com>
2021-02-06 22:37:17 +0100

    done

--- electronics/arduino/arduino-step-by-step-getting-started/section-8/arrays-example-2/arrays-example-2.ino
+++ electronics/arduino/arduino-step-by-step-getting-started/section-8/arrays-example-2/arrays-example-2.ino
@@ -6,6 +6,29 @@ int ledDuration[totalStates] = { 200, 400, 500, 100, 200, 50, 50, 500, 500 };
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  while(!Serial) { }
+
+  for (int i = 0; i < totalStates; i++) {
+    Serial.println("Please type 1 or 0 for patterns step");
+    Serial.print(i);
+    Serial.print(" out of ");
+    Serial.print(totalStates - 1);
+    Serial.print(" (default is 1");
+
+    while (Serial.available() == 0) { }
+
+    char input = Serial.read();
+    if (input == '1') {
+      ledState[i] = 1;
+    } else if(input == '0') {
+      ledState[i] = 1;
+    } else {
+      ledState[i] = 1;
+    }
+
+    Serial.println(ledState[i]);
+  }
 }
 
 void loop() {
