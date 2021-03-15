00886cd0f6613b003e4ea3fd1567cfb664251d14
Piotr Nowak <p.nowak2@gmail.com>
2021-03-16 00:25:51 +0100

    done

--- electronics/arduino/PlatformIO/src/main.cpp
+++ electronics/arduino/PlatformIO/src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+struct Player {
+  const char *name;
+};
+
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -8,8 +12,8 @@ void setup() {
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("Writing to led HIGH");
-  delay(1500);
+  delay(100);
   Serial.println("Writing to led LOW");
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1500);
+  delay(100);
 }
