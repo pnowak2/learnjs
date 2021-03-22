7ec4d965a0306904f269b44b48e953e4ea1c5c43
Piotr Nowak <p.nowak2@gmail.com>
2021-03-22 22:57:01 +0100

    done

---
+++ electronics/arduino/Elegoo The Most Complete Starter Kit/1-libraries-open-serial-monitor/1-libraries-open-serial-monitor.ino
@@ -0,0 +1,12 @@
+#include <IRremoteInt.h>
+#include <IRremote.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
