8cb7cd6ba9055e29bbb5f037cb7702167c6f3f1e
Piotr Nowak <p.nowak2@gmail.com>
2021-03-25 15:30:20 +0100

    done

---
+++ lang/typescript/handbook-4-2/specs/1.the-basics.spec.ts
@@ -0,0 +1,8 @@
+describe("1 Basic Types", () => {
+    describe("Boolean", () => {
+        it("should declare it", () => {
+            let msg: string = "BUBA";
+            expect(msg.toLowerCase()).toEqual('buba');
+        })
+    });
+})
