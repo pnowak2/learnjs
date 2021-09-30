d11d58cdb24551dca624673508108501e02b8b23
Piotr Nowak <p.nowak2@gmail.com>
2021-09-30 23:12:26 +0200

    done

--- electronics/arduino/Make an Arduino Robot - Udemy/src/main.cpp
+++ electronics/arduino/Make an Arduino Robot - Udemy/src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 
-int sensorPin = A2;
-int sensorValue = 0;
+int leftSensorPin = A2;
+int leftSensorValue = 0;
+int rightSensorPin = A0;
+int rightSensorValue = 0;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  sensorValue = analogRead(A2);
-  Serial.println(sensorValue);
-  delay(10);
+  leftSensorValue = analogRead(leftSensorPin);
+  rightSensorValue = analogRead(rightSensorPin);
+  Serial.print(leftSensorValue);
+  Serial.print(", ");
+  Serial.print(rightSensorValue);
+  Serial.println();
+  delay(100);
 }
