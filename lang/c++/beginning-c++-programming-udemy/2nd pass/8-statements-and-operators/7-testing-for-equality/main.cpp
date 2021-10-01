74f1bd60e4834651ba2a172b90e8e68aa3f2f564
Piotr Nowak <p.nowak2@gmail.com>
2021-10-01 16:28:57 +0200

    done

---
+++ lang/c++/beginning-c++-programming-udemy/2nd pass/8-statements-and-operators/7-testing-for-equality/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool equal_result {false};
+    bool not_equal_result {false};
+
+    int num1{}, num2{};
+
+    cout << "Enter two ints: ";
+    cin >> num1 >> num2;
+
+    equal_result = (num1 == num2);
+    not_equal_result = (num1 != num2);
+
+    cout << "Comparison result (equals): " << equal_result << endl;
+    cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+    cout << "Comparison result (equals): " << boolalpha << equal_result << endl;
+    cout << "Comparison result (not equals): " << boolalpha << not_equal_result << endl;
+
+    return 0;
+}
