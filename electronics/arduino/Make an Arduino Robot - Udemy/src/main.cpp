7d83d2911a7b8b74585de1a109b16671f1877ede
Piotr Nowak <p.nowak2@gmail.com>
2021-10-07 23:17:29 +0200

    done

--- electronics/arduino/Make an Arduino Robot - Udemy/src/main.cpp
+++ electronics/arduino/Make an Arduino Robot - Udemy/src/main.cpp
@@ -1,20 +1,100 @@
 #include <Arduino.h>
 
-int leftSensorPin = A2;
-int leftSensorValue = 0;
 int rightSensorPin = A0;
-int rightSensorValue = 0;
+int leftSensorPin = A2;
+int rightLedPin = 3;
+int leftLedPin = 2;
+int thresholdPot = A4;
 
-void setup() {
+int leftSensorValue = 0;
+int rightSensorValue = 0;
+int sensorDifference = 0;
+
+int closeness = 50;
+int threshold;
+
+void setup()
+{
+  pinMode(leftSensorPin, INPUT);
+  pinMode(rightSensorPin, INPUT);
+  pinMode(thresholdPot, INPUT);
+
+  pinMode(leftLedPin, OUTPUT);
+  pinMode(rightLedPin, OUTPUT);
+
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+  threshold = analogRead(thresholdPot);
+
+  Serial.print("Threshold: ");
+  Serial.print(threshold);
+  Serial.print("(");
+  Serial.print(threshold / 2);
+
+  Serial.print("), L=");
   leftSensorValue = analogRead(leftSensorPin);
-  rightSensorValue = analogRead(rightSensorPin);
   Serial.print(leftSensorValue);
-  Serial.print(", ");
+
+  Serial.print("), R=");
+  rightSensorValue = analogRead(rightSensorPin);
   Serial.print(rightSensorValue);
+
+  Serial.print(", Diff=");
+
+  sensorDifference = abs(leftSensorValue - rightSensorValue);
+
+  Serial.print(sensorDifference);
+  Serial.print(", Move: ");
+
+  if (leftSensorValue <= threshold &&
+      rightSensorValue <= threshold &&
+      leftSensorValue >= threshold / 2 &&
+      rightSensorValue >= threshold / 2 &&
+      sensorDifference <= closeness)
+  {
+    Serial.print("Forward");
+    digitalWrite(leftLedPin, HIGH);
+    digitalWrite(rightLedPin, HIGH);
+    delay(10);
+  }
+  else if (leftSensorValue >= threshold &&
+           rightSensorValue >= threshold &&
+           sensorDifference <= closeness)
+  {
+    Serial.print("STOP");
+    digitalWrite(leftLedPin, LOW);
+    digitalWrite(rightLedPin, LOW);
+    delay(10);
+  } else if (leftSensorValue < rightSensorValue &&
+           leftSensorValue >= threshold/2)
+  {
+    Serial.print("RIGHT");
+    digitalWrite(leftLedPin, HIGH);
+    digitalWrite(rightLedPin, LOW);
+    delay(10);
+  } else if (leftSensorValue > rightSensorValue &&
+           leftSensorValue >= threshold/2)
+  {
+    Serial.print("LEFT");
+    digitalWrite(leftLedPin, LOW);
+    digitalWrite(rightLedPin, HIGH);
+    delay(10);
+  } else if (leftSensorValue < threshold/2 &&
+           rightSensorValue < threshold/2)
+  {
+    Serial.print("LOST");
+    for (int i = 0; i < 10; i++) {
+      digitalWrite(leftLedPin, LOW);
+      digitalWrite(rightLedPin, LOW);
+      delay(50);
+      digitalWrite(leftLedPin, HIGH);
+      digitalWrite(rightLedPin, HIGH);
+      delay(50);
+    }
+  }
+
   Serial.println();
-  delay(100);
 }
