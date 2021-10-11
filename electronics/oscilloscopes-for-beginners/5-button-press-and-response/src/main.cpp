537eb2b0d7ddeaa1e67767fa28c544ebe904d3e2
Piotr Nowak <p.nowak2@gmail.com>
2021-10-11 22:18:10 +0200

    done

--- electronics/oscilloscopes-for-beginners/5-button-press-and-response/src/main.cpp
+++ electronics/oscilloscopes-for-beginners/5-button-press-and-response/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(A7, OUTPUT);
+  pinMode(A3, INPUT_PULLUP);
+  digitalWrite(A7, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  const PinStatus status = digitalRead(A3);
+  if(status == LOW) {
+    digitalWrite(A7, HIGH);
+  } else {
+    digitalWrite(A7, LOW);
+  }
 }
