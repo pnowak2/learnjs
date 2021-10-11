5908bbd3f961a9ffa9b7be3b2dfb81a85bfc9f68
Piotr Nowak <p.nowak2@gmail.com>
2021-10-11 23:01:47 +0200

    done

---
+++ electronics/arduino/Make an Arduino Robot - Udemy/src/ultrasonic-sensor-setup.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Ultrasonic.h>
+
+/*
+ * Pass as a parameter the trigger and echo pin, respectively,
+ * or only the signal pin (for sensors 3 pins), like:
+ * Ultrasonic ultrasonic(13);
+ */
+Ultrasonic ultrasonic(A3, A2);
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Pass INC as a parameter to get the distance in inches
+  distance = ultrasonic.read();
+  
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+  delay(1000);
+}
