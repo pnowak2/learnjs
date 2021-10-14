66f31387f3d0a234fccb0f003f7938e8a7ea25fa
Piotr Nowak <p.nowak2@gmail.com>
2021-10-14 22:57:44 +0200

    done

---
+++ electronics/arduino/Make an Arduino Robot - Udemy/src/distance-servo-setup.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo rightMotor;
+
+void setup() {
+  rightMotor.attach(A5);
+  rightMotor.write(90);
+}
+
+void loop() {
+  rightMotor.write(0);
+  delay(1000);
+  rightMotor.write(180);
+  delay(1000);
+  rightMotor.write(90);
+  delay(1000);
+}
--- electronics/arduino/Make an Arduino Robot - Udemy/src/wheel-servos-setup.cpp
+++ electronics/arduino/Make an Arduino Robot - Udemy/src/wheel-servos-setup.cpp
@@ -1,35 +1,35 @@
-#include <Arduino.h>
-#include <Servo.h>
+// #include <Arduino.h>
+// #include <Servo.h>
 
-const int RForward = 120;
-const int RBackward = 60;
+// const int RForward = 120;
+// const int RBackward = 60;
 
-const int LForward = 120;
-const int LBackward = 60;
+// const int LForward = 120;
+// const int LBackward = 60;
 
-const int RNeutral = 90;
-const int LNeutral = 90;
+// const int RNeutral = 90;
+// const int LNeutral = 90;
 
-Servo leftMotor;
-Servo rightMotor;
+// Servo leftMotor;
+// Servo rightMotor;
 
-void setup() {
-  rightMotor.attach(11);
-  leftMotor.attach(10);
-}
+// void setup() {
+//   rightMotor.attach(11);
+//   leftMotor.attach(10);
+// }
 
-void loop() {
-  leftMotor.write(LNeutral);
-  rightMotor.write(RNeutral);
+// void loop() {
+//   leftMotor.write(LNeutral);
+//   rightMotor.write(RNeutral);
 
-  // leftMotor.write(LForward);
-  // rightMotor.write(RForward);
+//   // leftMotor.write(LForward);
+//   // rightMotor.write(RForward);
 
-  // leftMotor.write(LBackward);
-  // rightMotor.write(RForward);
+//   // leftMotor.write(LBackward);
+//   // rightMotor.write(RForward);
 
-  // leftMotor.write(LForward);
-  // rightMotor.write(RBackward);
+//   // leftMotor.write(LForward);
+//   // rightMotor.write(RBackward);
 
-  // delay(1500);
-}
+//   // delay(1500);
+// }
