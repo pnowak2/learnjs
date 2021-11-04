0315f3f8d91a608eb06c0a1b1df28f1756d92cd3
Piotr Nowak <p.nowak2@gmail.com>
2021-11-04 08:39:42 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/specs/test_11_files.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/specs/test_11_files.py
@@ -2,38 +2,73 @@ from typing import TextIO
 
 
 class TestFiles:
-    def test_read_file(self):
-        my_file = open('excercises/13-file-io/test.txt')
-        assert my_file.read() == 'hello world!'
+    class TestRead:
+        def test_read_file(self):
+            my_file = open('excercises/13-file-io/test.txt')
+            assert my_file.read() == 'hello world!'
 
-        my_file.close()
+            my_file.close()
 
-    def test_cursor(self):
-        my_file = open('excercises/13-file-io/test.txt')
-        assert my_file.read() == 'hello world!'
-        assert my_file.read() == ''
+        def test_cursor(self):
+            my_file = open('excercises/13-file-io/test.txt')
+            assert my_file.read() == 'hello world!'
+            assert my_file.read() == ''
 
-        my_file.seek(0)
+            my_file.seek(0)
 
-        assert my_file.read() == 'hello world!'
+            assert my_file.read() == 'hello world!'
 
-        my_file.close()
+            my_file.close()
 
-    def test_readline(self):
-        my_file = open('excercises/13-file-io/multiline.txt')
-        assert my_file.readline() == 'first line\n'
-        assert my_file.readline() == 'second line\n'
-        assert my_file.readline() == 'third line'
-        assert my_file.readline() == ''
+        def test_readline(self):
+            my_file = open('excercises/13-file-io/multiline.txt')
+            assert my_file.readline() == 'first line\n'
+            assert my_file.readline() == 'second line\n'
+            assert my_file.readline() == 'third line'
+            assert my_file.readline() == ''
 
-        my_file.close()
+            my_file.close()
 
-    def test_readlines(self):
-        my_file = open('excercises/13-file-io/multiline.txt')
-        assert my_file.readlines() == [
-            'first line\n',
-            'second line\n',
-            'third line'
-        ]
+        def test_readlines(self):
+            my_file = open('excercises/13-file-io/multiline.txt')
+            assert my_file.readlines() == [
+                'first line\n',
+                'second line\n',
+                'third line'
+            ]
 
-        my_file.close()
+            my_file.close()
+
+    class TestReadWriteAppend:
+        def test_read_with_statement(self):
+            with open('excercises/13-file-io/test.txt', mode='r') as file:
+                assert file.read() == 'hello world!'
+
+        def test_readwrite_with_statement(self):
+            with open('excercises/13-file-io/readwrite.txt', mode='r+') as file:
+                file.truncate()
+                file.write('hello')
+
+                file.seek(0)
+                assert file.read() == 'hello'
+
+                file.write('world')
+                file.seek(0)
+                assert file.read() == 'helloworld'
+
+        def test_append_with_statement(self):
+            '''no overrides, always adds at end of the file'''
+            with open('excercises/13-file-io/append.txt', mode='a') as file:
+                file.write('hello')
+                file.write('world')
+
+        def test_write_with_statement(self):
+            '''will replace file with new contents, not just first bytes'''
+            with open('excercises/13-file-io/write.txt', mode='w') as file:
+                file.write('yo')
+                file.write('man')
+
+        def test_write_file_which_does_not_exist(self):
+            '''will create new file even if it didnt exist before'''
+            with open('excercises/13-file-io/doesnotexist.txt', mode='w') as file:
+                file.write('now you do!')
