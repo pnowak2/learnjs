d05d0c5c5be6f6dd096874fdc47e2906839c1e5f
Piotr Nowak <p.nowak2@gmail.com>
2021-11-05 12:19:04 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/13-file-io/translator.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/13-file-io/translator.py
@@ -1,21 +1,33 @@
 from pathlib import Path, PosixPath
 from googletrans import Translator
+import re
 import codecs
 
-langs = [
-            'bg', 'cs', 'da', 'de', 'et', 'el', 'en', 'es',
-            'fi', 'fr', 'ga', 'hr', 'hu', 'it', 'lt', 'lv',
-            'mt', 'nl', 'pl', 'pt', 'ro', 'sk', 'sl', 'sv',
-        ]
+langs = [ 'bg' ]
+# langs = [
+#             'bg', 'cs', 'da', 'de', 'et', 'el', 'en', 'es',
+#             'fi', 'fr', 'ga', 'hr', 'hu', 'it', 'lt', 'lv',
+#             'mt', 'nl', 'pl', 'pt', 'ro', 'sk', 'sl', 'sv',
+#         ]
 
 translator = Translator()
 path = Path('en.json')
-with open(path.as_posix(), mode='r') as file:
-    sources = file.readlines()
-    src = ''.join(sources)
 
-    for lang in langs:
-        translation = translator.translate(src, dest=lang)
-        with codecs.open(f'translations/{lang}.json', mode='w', encoding='utf-8') as file:
-            print(f'Writing {lang} json translation..')
-            file.write(translation.text)
+with open(path.as_posix(), mode='r') as file:
+    src_lines = file.readlines()
+    regex = r'(^\".*:).*(\".*\")'
+    pattern = re.compile(regex)
+    src_match = pattern.search('"ecl.app-shell.ECL-APP-SHELL": "ECL Application Shell",')
+
+    src_key, src_value = (src_match.group(1), src_match.group(2))
+
+    # print(f'key: {src_key}, value: {src_value}')
+
+    s = re.sub(regex, r"\1" + ' buba', '"ecl.app-shell.ECL-APP-SHELL": "ECL Application Shell",')
+    print(s)
+
+    # for lang in langs:
+        # translation = translator.translate(src, dest=lang)
+        # with codecs.open(f'translations/{lang}.json', mode='w', encoding='utf-8') as file:
+        #     print(f'Writing {lang} json translation..')
+        #     file.write(translation.text)
---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/specs/test_12_regexp.py
@@ -0,0 +1,42 @@
+import re
+
+class TestRegularExpressions:
+    def test_intro(self):
+        string = 'search inside of this text please!'
+
+        assert ('search' in string) == True
+
+    def test_search_for_string_pattern(self):
+        string = 'search inside of this text please!'
+
+        match = re.search('this', string)
+
+        assert(match.span()) == (17, 21)
+        assert(match.start()) == 17
+        assert(match.end()) == 21 
+        assert(match.group()) == 'this' 
+        
+    def test_search_for_pattern_with_re_compile(self):
+        string = 'search inside of this text please this!'
+
+        pattern = re.compile('this')
+        match = pattern.search(string)
+        found_items = pattern.findall(string)
+        is_full_match = pattern.fullmatch(string)
+
+        assert(match.span()) == (17, 21)
+        assert(match.start()) == 17
+        assert(match.end()) == 21 
+        assert(match.group()) == 'this' 
+        assert found_items == ['this', 'this']
+        assert is_full_match == None
+
+    def test_patterns(self):
+        string = 'search inside of this text please this!'
+
+        pattern = re.compile(r"([a-zA-Z]).(a)")
+        a = pattern.search(string)
+
+        assert a.group() == 'sea'
+        assert a.group(1) == 's'
+        assert a.group(2) == 'a'
