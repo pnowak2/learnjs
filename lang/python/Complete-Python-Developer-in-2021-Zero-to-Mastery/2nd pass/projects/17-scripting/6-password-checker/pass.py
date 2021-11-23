cc6d0b5be463ab3e9697e53dd9a19e68c844bade
Piotr Nowak <p.nowak2@gmail.com>
2021-11-23 12:04:18 +0100

    done

---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/17-scripting/6-password-checker/pass.py
@@ -0,0 +1,20 @@
+import requests
+import hashlib
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}')
+    
+    return res
+
+    
+
+def pwned_api_check(password):
+    shapass = hashlib.sha1( password.encode('utf-8') ).hexdigest().upper()
+    return shapass
+
+passwd = pwned_api_check('pass')
+res = request_api_data(passwd[:5])
+print(res)
