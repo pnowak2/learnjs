417e6c95ebd0e527915c61101a4740792944d9f1
Piotr Nowak <p.nowak2@gmail.com>
2021-11-25 10:52:32 +0100

    done

---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/18-scraping-data/scrape.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+response = requests.get('http://news.ycombinator.com/news')
+soup = BeautifulSoup(response.text, 'html.parser')
+links = soup.select('.titlelink')
+subtext = soup.select('.subtext')
+
+def create_custom_hn(links, subtext, rank=100):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
+
+    return sorted(
+        list(filter(lambda item : item['votes'] > rank, hn)), 
+        key=lambda item: item['votes'], 
+        reverse=True
+    )
+
+pprint.pprint(create_custom_hn(links, subtext, 200))
