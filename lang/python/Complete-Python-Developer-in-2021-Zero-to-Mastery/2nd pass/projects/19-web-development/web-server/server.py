a217e2cb5e5cc66a356d06c849b2e859a255dbee
Piotr Nowak <p.nowak2@gmail.com>
2021-11-26 13:20:02 +0100

    done

---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/19-web-development/web-server/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <code>
+      <pre>
+      setup venv to web folder
+      set FLASK_APP=server.py
+      flask run
+      </pre>
+    </code>
+    '''
