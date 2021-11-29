94324a8eb35da2b491c05154db8d8a520309bd83
Piotr Nowak <p.nowak2@gmail.com>
2021-11-29 13:48:04 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/19-web-development/web-server/server.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/2nd pass/projects/19-web-development/web-server/server.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
+    return render_template('./index.html')
+
+@app.route('/<username>/<int:id>')
+def user(username=None, id=None):
+    return render_template('./index.html', name=username, id=id)
+
+@app.route('/about')
+def about():
+    return render_template('./about.html')
+
+@app.route('/env')
+def environment():
     return '''
     <code>
       <pre>
@@ -13,4 +25,16 @@ def hello_world():
       flask run
       </pre>
     </code>
+    '''
+
+@app.route('/blog')
+def blog():
+    return '''
+    Blog
+    '''
+
+@app.route('/blog/2020/dogs')
+def blogs_2020():
+    return '''
+    this is my dog
     '''
