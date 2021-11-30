952fccdf388f7d52b927fbbc7dc867fb88986150
Piotr Nowak <p.nowak2@gmail.com>
2021-11-30 15:21:37 +0100

    done

--- lang/c++/beginning-c++-programming-udemy/2nd pass/17-smart-pointers/3-shared-pointers/main.cpp
+++ lang/c++/beginning-c++-programming-udemy/2nd pass/17-smart-pointers/3-shared-pointers/main.cpp
@@ -25,6 +25,44 @@ public:
     }
 };
 
+void func(Test t) {
+    cout << "func: " << t.get_data() << endl;
+}
+
 int main(int argc, char *argv []) {
+    shared_ptr<int> p1 {new int(100)};
+    cout << "Use count " << p1.use_count() << endl;
+
+    shared_ptr<int> p2 {p1};
+
+    cout << "Use count " << p1.use_count() << endl;
+
+    p1.reset();
+
+    cout << "p1 Use count " << p1.use_count() << endl;
+    cout << "p2 Use count " << p2.use_count() << endl;
+
+    cout << "====" << endl;
+
+    shared_ptr<Test> ptr = make_shared<Test>(100);
+    func(*ptr.get());
+
+    cout << "use count: " << ptr.use_count() << endl;
+
+    {
+        shared_ptr<Test> ptr1 = ptr;
+        cout << "use count: " << ptr.use_count() << endl;
+        {
+            shared_ptr<Test> ptr2 = ptr;
+            cout << "use count: " << ptr.use_count() << endl;
+            ptr.reset();
+        }
+        cout << "use count: " << ptr.use_count() << endl;
+    }
+
+    cout << "use count: " << ptr.use_count() << endl;
+
+    cout << "====" << endl;
+
     return 0;
 }
