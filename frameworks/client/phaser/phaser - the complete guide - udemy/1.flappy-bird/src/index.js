46cdde6890e84233906339e1e9a16e8e2bd8df85
Piotr Nowak <p.nowak2@gmail.com>
2022-02-21 15:36:14 +0100

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/index.js
@@ -19,8 +19,12 @@ const config = {
 }
 
 let bird = null;
+const initialBirdPosition = {
+  x: config.width / 10,
+  y: config.height / 2
+};
 const VELOCITY = 200;
-let flapVelocity = 300;
+const flapVelocity = 300;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -29,13 +33,22 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
 }
 
 function update(time, delta) {
+  if (bird.y > config.height || bird.y < -bird.height) {
+    restartBirdPosition();
+  }
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
 }
 
 function flap() {
