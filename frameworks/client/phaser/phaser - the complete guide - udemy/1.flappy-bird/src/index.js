ffd1749aca58a46427a2b5617a9b3733eca9f981
Piotr Nowak <p.nowak2@gmail.com>
2022-02-22 15:41:07 +0100

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/index.js
@@ -8,7 +8,6 @@ const config = {
     default: 'arcade',
     arcade: {
       debug: true,
-      gravity: { y: 400 },
     },
   },
   scene: {
@@ -19,24 +18,48 @@ const config = {
 }
 
 let bird = null;
+let upperPipe = null;
+let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
+const pipeVerticalDistanceRange = [150, 250];
+
 const initialBirdPosition = {
   x: config.width / 10,
   y: config.height / 2
 };
+
 const VELOCITY = 200;
 const flapVelocity = 300;
+const PIPES_TO_RENDER = 4;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
+
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
+  bird.body.gravity.y = 400;
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
 
   this.input.on('pointerdown', flap);
-  this.input.keyboard.on('keydown_SPACE', flap);
+  this.input.keyboard.on('keydown-SPACE', flap);
 }
 
 function update(time, delta) {
