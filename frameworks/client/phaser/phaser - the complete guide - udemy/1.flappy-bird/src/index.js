786a4d9a903bc9adcb7488d550160514720b714d
Piotr Nowak <p.nowak2@gmail.com>
2022-02-23 10:43:26 +0100

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/index.js
@@ -18,8 +18,6 @@ const config = {
 }
 
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
 let pipeHorizontalDistance = 0;
 
 const pipeVerticalDistanceRange = [150, 250];
@@ -46,16 +44,10 @@ function create() {
   bird.body.gravity.y = 400;
 
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    pipeHorizontalDistance += 400;
+    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
 
-    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
-
-    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-    lowerPipe = this.physics.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
-
-    upperPipe.body.velocity.x = -200;
-    lowerPipe.body.velocity.x = -200;
+    placePipe(upperPipe, lowerPipe);
   }
 
   this.input.on('pointerdown', flap);
@@ -66,6 +58,22 @@ function update(time, delta) {
   if (bird.y > config.height || bird.y < -bird.height) {
     restartBirdPosition();
   }
+}
+
+function placePipe(uPipe, lPipe) {
+    pipeHorizontalDistance += 400;
+
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+
+    uPipe.x = pipeHorizontalDistance;
+    uPipe.y = pipeVerticalPosition;
+
+    lPipe.x = uPipe.x;
+    lPipe.y = uPipe.y + pipeVerticalDistance;
+
+    uPipe.body.velocity.x = -200;
+    lPipe.body.velocity.x = -200;
 }
 
 function restartBirdPosition() {
