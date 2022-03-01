df0800ac71d1aed729ca62c66b7512f989e4093b
Piotr Nowak <p.nowak2@gmail.com>
2022-03-01 15:26:06 +0100

    done

--- libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/2-nd pass/cryptochain/block.js
+++ libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/2-nd pass/cryptochain/block.js
@@ -7,11 +7,4 @@ class Block {
     }
 }
 
-const block1 = new Block({
-    timestamp: '01/01/01',
-    lastHash: 'foo-lastHash',
-    hash: 'foo-hash',
-    data: 'foo-data'
-});
-
-console.log('block1', block1);
+module.exports = Block;
