439f603a650c6d8a3bbbe0be2a24f0ec3ec19d7f
Piotr Nowak <p.nowak2@gmail.com>
2022-03-01 13:12:39 +0100

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/scenes/menu.scene.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy/1.flappy-bird/src/scenes/menu.scene.js
@@ -3,5 +3,17 @@ import BaseScene from './base.scene';
 export default class MenuScene extends BaseScene {
     constructor(config) {
         super('MenuScene', config);
+
+        this.menu = [
+            { scene: 'PlayScene', text: 'Play' },
+            { scene: 'ScoreScene', text: 'Score' },
+            { scene: null, text: 'Exit' },
+        ];
+    }
+
+    create() {
+        super.create();
+
+        this.createMenu(this.menu);
     }
 }
