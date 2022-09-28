3f5132364f1e4fbd1d12e001d59dfeaa10aacc60
Piotr Nowak <p.nowak2@gmail.com>
2022-09-28 12:32:01 +0200

    done

---
+++ lang/c++/the-modern-cpp-20-masterclass/8-variables-and-data-types/2-decimals-and-integers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+    int hex = 0xf;
+    int dec = 15;
+    int oct = 017;
+    int bin = 0b00001111;
+
+    std::cout << hex << std::endl;
+    std::cout << dec << std::endl;
+    std::cout << oct << std::endl;
+    std::cout << bin << std::endl;
+
+    return 0;
+}
