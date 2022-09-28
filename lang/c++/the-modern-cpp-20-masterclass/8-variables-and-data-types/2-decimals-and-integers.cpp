4aa103efe543b09982433727a1a2d385ef08bf98
Piotr Nowak <p.nowak2@gmail.com>
2022-09-28 12:50:34 +0200

    done

--- lang/c++/the-modern-cpp-20-masterclass/8-variables-and-data-types/2-decimals-and-integers.cpp
+++ lang/c++/the-modern-cpp-20-masterclass/8-variables-and-data-types/2-decimals-and-integers.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
 int main () {
-    int hex = 0xf;
-    int dec = 15;
-    int oct = 017;
-    int bin = 0b00001111;
+    int elephant_count;
+    int lion_count {};
+    int dog_count {10};
+    int cat_count {15};
+    int ants (5); // functional initialization
 
-    std::cout << hex << std::endl;
-    std::cout << dec << std::endl;
-    std::cout << oct << std::endl;
-    std::cout << bin << std::endl;
+    int truncated (5.9);
+    int another_trunc = 7.6;
+    // int truncated_init { 5.9 }; // wont compile, narrowing conversion error
+
+    int animals {dog_count + cat_count};
+
+    std::cout << truncated << std::endl;
+    std::cout << another_trunc << std::endl;
+
+    std::cout << sizeof(elephant_count) << std::endl;
 
     return 0;
 }
