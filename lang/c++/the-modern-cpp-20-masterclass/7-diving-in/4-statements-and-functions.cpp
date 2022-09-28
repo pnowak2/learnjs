8fe981c52dbe32d294366e0a2827367d4aa312f9
Piotr Nowak <p.nowak2@gmail.com>
2022-09-28 11:02:17 +0200

    done

--- lang/c++/the-modern-cpp-20-masterclass/7-diving-in/4-statements-and-functions.cpp
+++ lang/c++/the-modern-cpp-20-masterclass/7-diving-in/4-statements-and-functions.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+int addNumbers(int first, int second) {
+    int result = first + second;
+    return result;
+}
+
 int main () {
+    int first_num {3};
+    int second_num {5};
+
+    std::cout << first_num << second_num;
+
+    int sum = addNumbers(3, 5);
+    std::cout << std::endl;
+    std::cout << sum;
+
+    int sum2 = addNumbers(17, 16);
+    std::cout << std::endl;
+    std::cout << sum2;
+
     return 0;
 }
