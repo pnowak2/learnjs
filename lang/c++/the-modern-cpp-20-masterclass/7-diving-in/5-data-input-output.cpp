3e044b69f0cb5c9b9ee7d8d953056171e08d2730
Piotr Nowak <p.nowak2@gmail.com>
2022-09-28 12:15:12 +0200

    done

--- lang/c++/the-modern-cpp-20-masterclass/7-diving-in/5-data-input-output.cpp
+++ lang/c++/the-modern-cpp-20-masterclass/7-diving-in/5-data-input-output.cpp
@@ -14,10 +14,23 @@ int main () {
     std::string name;
 
     std::cout << "Your name and age: ";
-    std::cin >> name;
-    std::cin >> age2;
+
+    // std::cin >> name;
+    // std::cin >> age2;
+    std::cin >> name >> age2;
 
     std::cout << "Hello " << name << ", you are " << age2;
+
+    std::string full_name;
+
+    std::cout << std::endl;
+
+    std::cout << "Your full name and age: ";
+
+    std::getline(std::cin, full_name);
+    std::cin >> age2;
+
+    std::cout << "Hello " << full_name << ", you are " << age2;
 
     return 0;
 }
