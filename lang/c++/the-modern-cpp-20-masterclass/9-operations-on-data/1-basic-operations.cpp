8cfcce6cd5ae56c59d01eb4b1663fbcc710e8061
Piotr Nowak <p.nowak2@gmail.com>
2022-10-20 10:51:02 +0200

    done

--- lang/c++/the-modern-cpp-20-masterclass/9-operations-on-data/1-basic-operations.cpp
+++ lang/c++/the-modern-cpp-20-masterclass/9-operations-on-data/1-basic-operations.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main () {
+int main()
+{
+    int num1{2};
+    int num2{7};
+
+    int result = num1 + num2;
+
+    std::cout << "result: " << result << std::endl;
 
     return 0;
 }
