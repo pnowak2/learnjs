e38d918f1b0969ea088f93e396ce39b85e5aeb38
Piotr Nowak <p.nowak2@gmail.com>
2022-11-24 16:43:59 +0100

    done

--- lang/c++/the-modern-cpp-20-masterclass/8-variables-and-data-types/1-number-systems.cpp
+++ lang/c++/the-modern-cpp-20-masterclass/8-variables-and-data-types/1-number-systems.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 
 int main () {
-    int hex = 0xf;
-    int dec = 15;
-    int oct = 017;
-    int bin = 0b00001111;
-
-    std::cout << hex << std::endl;
-    std::cout << dec << std::endl;
-    std::cout << oct << std::endl;
-    std::cout << bin << std::endl;
+    // endl
+    std::cout << "Hello" << std::endl;
+    std::cout << "Hello\n";
 
     return 0;
 }
--- lang/c++/the-modern-cpp-20-masterclass/9-operations-on-data/6-logical-operators.cpp
+++ lang/c++/the-modern-cpp-20-masterclass/9-operations-on-data/6-logical-operators.cpp
@@ -5,10 +5,13 @@ int main()
     bool a = true;
     bool b = false;
 
+    std::cout << std::boolalpha;
+
     std::cout << (a && b) << std::endl;
     std::cout << (a || b) << std::endl;
     std::cout << (a && ~b) << std::endl;
     std::cout << (a ^ b) << std::endl;
+    std::cout << !(a ^ b) << std::endl;
 
     return 0;
 }
