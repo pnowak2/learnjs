f02a2d8f405c8b277f31a8f6165cc7fb7ff47e9c
Piotr Nowak <p.nowak2@gmail.com>
2023-03-23 14:38:47 +0100

    done

---
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/2022/3-basics/3.5-styles-tags-shadow-dom/tooltip.js
@@ -0,0 +1,50 @@
+class EuiTooltip extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open'});
+
+        this._tooltipContainer;
+        this._tooltipText = 'some dummy tooltip text';
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: black;
+                    color: white;
+                    position: absolute;
+                    z-index: 10;
+                }
+            </style>
+
+            <slot>Default slot content</slot>
+            <span> (?)</span>
+        `;
+    }
+
+    connectedCallback() {
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+
+        const tooltipIcon = this.shadowRoot.querySelector('span');
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+
+        this.shadowRoot.appendChild(tooltipIcon);
+
+        this.style.position = 'relative';
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tooltipText;
+
+        this.shadowRoot.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.shadowRoot.removeChild(this._tooltipContainer);
+    }
+}
+
+customElements.define('eui-tooltip', EuiTooltip);
