6100cffe28ef1c7161d9e42f57c9d87ffbbe3c97
Piotr Nowak <p.nowak2@gmail.com>
2023-03-27 16:08:05 +0200

    done

---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/3rd pass/specs/test_3_python_basics_I.py
@@ -0,0 +1,13 @@
+class TestDataTypes:
+    def test_fundamental_data_types(self):
+        int
+        float
+        bool
+        str
+        list
+        tuple
+        set
+        dict
+
+        #classes
+        #specialized data types (packages/modules from libraries)
