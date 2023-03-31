31e7efc00eda092e44ab623b5b1c9e008d762bbd
Piotr Nowak <p.nowak2@gmail.com>
2023-03-31 10:41:17 +0200

    done

--- libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/3-rd pass/blockchain/block.test.js
+++ libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/3-rd pass/blockchain/block.test.js
@@ -32,5 +32,26 @@ describe('genesis()', () => {
     it('returns appropriate genesis properties', () => {
         expect(genesisBlock).toEqual(GENESIS_DATA);
     });
-    
+});
+
+describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const minedBlock = Block.mineBlock({ lastBlock, data })
+
+    it('returns a Block instance', () => {
+        expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be `hash` of last block', () => {
+        expect(minedBlock.lastHash).toEqual(lastBlock.hash)
+    });
+
+    it('sets the data', () => {
+        expect(minedBlock.data).toEqual(data);
+    });
+
+    it('sets a timestamp', () => {
+        expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
 });
