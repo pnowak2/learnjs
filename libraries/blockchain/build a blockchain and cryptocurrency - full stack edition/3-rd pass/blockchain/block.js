146553ab1d6f34f7858bae4496ba8e70764f28bc
Piotr Nowak <p.nowak2@gmail.com>
2023-03-31 09:55:21 +0200

    done

--- libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/3-rd pass/blockchain/block.js
+++ libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/3-rd pass/blockchain/block.js
@@ -8,12 +8,3 @@ class Block {
 }
 
 module.exports = Block;
-
-const block1 = new Block({
-    data: 'foo-data',
-    lastHash: 'foo-lastHash',
-    hash: 'foo-hash',
-    timestamp: '01/01/01',
-});
-
-console.log('block1', block1);
--- libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/3-rd pass/blockchain/block.test.js
+++ libraries/blockchain/build a blockchain and cryptocurrency - full stack edition/3-rd pass/blockchain/block.test.js
@@ -1,7 +1,30 @@
 const Block = require('./block');
 
 describe('Block', () => {
-    it('should..', () => {
+    const timestamp = 'a-date';
+    const lastHash = 'foo-hash';
+    const hash= 'foo-hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block({
+        timestamp,
+        lastHash,
+        hash,
+        data
+    });
 
+    it('has a timestamp, lashHash, hash, data', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
     });
 });
+
+// const block1 = new Block({
+//     data: 'foo-data',
+//     lastHash: 'foo-lastHash',
+//     hash: 'foo-hash',
+//     timestamp: '01/01/01',
+// });
+
+// console.log('block1', block1);
