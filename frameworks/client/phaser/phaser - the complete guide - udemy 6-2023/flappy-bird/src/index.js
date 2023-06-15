97bd2278b9af26b08c74284ba7a12b6f7abe7297
Piotr Nowak <p.nowak2@gmail.com>
2023-06-15 16:51:37 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/index.js
@@ -19,7 +19,9 @@ const config = {
 };
 
 const VELOCITY = 200;
-let flapVelocity = 300;
+const FLAP_VELOCITY = 300;
+const INITIAL_BIRD_POSITION = { x: config.width * 0.1, y: config.height / 2 };
+
 let bird = null;
 
 function preload() {
@@ -29,21 +31,26 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(INITIAL_BIRD_POSITION.x, INITIAL_BIRD_POSITION.y, 'bird').setOrigin(0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown-SPACE', flap);
 }
 
 function update(time, delta) {
-  if(bird.y > config.height || (bird.y + bird.height) <= 0) {
-    bird.y = config.height / 2;
-    bird.body.velocity.y = 0;
+  if (bird.y > config.height || (bird.y + bird.height) <= 0) {
+    restartPlayerPosition();
   }
 }
 
 function flap() {
-  bird.body.velocity.y -= flapVelocity;
+  bird.body.velocity.y -= FLAP_VELOCITY;
+}
+
+function restartPlayerPosition() {
+  bird.x = INITIAL_BIRD_POSITION.x;
+  bird.y = INITIAL_BIRD_POSITION.y;
+  bird.body.velocity.y = 0;
 }
 
 new Phaser.Game(config);
