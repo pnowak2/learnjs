8912c53ad77738425b4beb2c70e91323b024eebe
Piotr Nowak <p.nowak2@gmail.com>
2023-06-15 11:37:00 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/index.js
@@ -5,21 +5,40 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
   },
   scene: {
     preload,
     create,
+    update,
   }
 };
 
+let bird = null;
+let totalDelta = null;
+
 function preload() {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
 }
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0, 0);
 }
 
+function update(time, delta) {
+  totalDelta += delta;
+
+  if (totalDelta < 1000) {
+    return;
+  }
+
+  // console.log(bird.body.velocity.y);
+  totalDelta = 0;
+}
 
 new Phaser.Game(config);
