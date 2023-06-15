132757475e7a687163f32433beb9403d9f425e21
Piotr Nowak <p.nowak2@gmail.com>
2023-06-16 00:11:39 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/index.js
@@ -26,6 +26,7 @@ const PIPE_VERTICAL_DISTANCE_RANGE = [100, 250];
 
 let bird = null;
 let pipeHorizontalDistance = 0;
+let pipes = null;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -39,12 +40,16 @@ function create() {
   bird = this.physics.add.sprite(INITIAL_BIRD_POSITION.x, INITIAL_BIRD_POSITION.y, 'bird').setOrigin(0);
   bird.body.gravity.y = 400;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
-    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
 
     placePipe(upperPipe, lowerPipe);
   }
+
+  pipes.setVelocityX(-VELOCITY);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown-SPACE', flap);
@@ -66,9 +71,6 @@ function placePipe(uPipe, lPipe) {
 
     lPipe.x = uPipe.x;
     lPipe.y = uPipe.y + pipeVerticalDistance;
-
-    uPipe.body.velocity.x = -VELOCITY;
-    lPipe.body.velocity.x = -VELOCITY;
 }
 
 function flap() {
