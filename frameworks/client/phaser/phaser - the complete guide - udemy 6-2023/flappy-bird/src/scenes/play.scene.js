e6a7057b5370ea2b586abc116666ee36d8af275d
Piotr Nowak <p.nowak2@gmail.com>
2023-06-17 22:39:32 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/scenes/play.scene.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/scenes/play.scene.js
@@ -195,14 +195,36 @@ class PlayScene extends BaseScene {
   }
 
   listenToEvents() {
-    this.events.on('resume', () => {
+    if (this.pauseEvent) { return }
+
+    this.pauseEvent = this.events.on('resume', () => {
       this.initialTime = 3;
       this.countDownText = this.add.text(
         ...this.screenCenter,
         'Fly in ' + this.initialTime,
         this.fontOptions
       ).setOrigin(0.5);
+
+      this.timedEvent = this.time.addEvent({
+        delay: 1000,
+        callback: this.countDown,
+        callbackScope: this,
+        loop: true
+      });
     });
+  }
+
+  countDown() {
+    this.initialTime -= 1;
+    this.countDownText.setText('Fly in ' + this.initialTime);
+
+    if (this.initialTime < 0) {
+      this.countDownText.setText('');
+      this.timedEvent.remove();
+
+      this.physics.resume();
+      this.scene.resume();
+    }
   }
 }
 
