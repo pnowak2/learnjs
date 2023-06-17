6285ab106e636f6f4cd8c5f6e26e1e5583b54f82
Piotr Nowak <p.nowak2@gmail.com>
2023-06-17 15:37:47 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/scenes/menu.scene.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 6-2023/flappy-bird/src/scenes/menu.scene.js
@@ -3,11 +3,28 @@ import BaseScene from './base.scene';
 class MenuScene extends BaseScene {
   constructor(config) {
     super('MenuScene', config);
+
+    this.menu = [
+      { scene: 'PlayScene', text: 'Play' },
+      { scene: 'ScoreScene', text: 'Score' },
+      { scene: null, text: 'Exit' },
+    ]
   }
 
   create() {
     super.create();
-    this.scene.start('PlayScene');
+    this.createMenu(this.menu, this.setupMenuEvents);
+  }
+
+  setupMenuEvents(menuItem) {
+    const textGO = menuItem.textGO;
+    textGO.on('pointerover', () => {
+      textGO.setStyle({ fill: '#ff0' })
+    });
+
+    textGO.on('pointerout', () => {
+      textGO.setStyle({ fill: '#fff' })
+    })
   }
 }
 
