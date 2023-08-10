bac20cc20823f9832ff64f384c58aa45c7b6c4e8
Piotr Nowak <p.nowak2@gmail.com>
2023-08-10 10:44:34 +0200

    done

---
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/2023/4-diving-deeper/4.6-styling-host-conditional/tooltip.js
@@ -0,0 +1,72 @@
+class EuiTooltip extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+
+        this._tooltipContainer;
+        this._tooltipText = 'some dummy text';
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: black;
+                    color: white;
+                    position: absolute;
+                    z-index: 10;
+                }
+
+                .icon {
+                    background: black;
+                    display: inline-flex;
+                    justify-content: center;
+                    border-radius: 32px;
+                    color: white;
+                    width: 16px;
+                    height: 16px;
+                }
+
+                :host {
+                    border: 1px dotted red;
+                }
+
+                :host(.eui-important) {
+                    background: green;
+                }
+
+                :host-context(p) {
+                    background: violet;
+                }
+
+                ::slotted(.highlight) {
+                    color: red;
+                }
+            </style>
+            <slot>Default slot</slot>
+            <span class="icon">?</span>
+        `;
+    }
+
+    connectedCallback() {
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+        const tooltipIcon = this.shadowRoot.querySelector('span');
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+
+        this.shadowRoot.appendChild(tooltipIcon);
+        this.style.position = 'relative';
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.shadowRoot.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.shadowRoot.removeChild(this._tooltipContainer);
+    }
+}
+
+customElements.define('eui-tooltip', EuiTooltip);
