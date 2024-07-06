366719bc6f120e5bc16d59924a971a2a6fdb3b52
Piotr Nowak <p.nowak2@gmail.com>
2024-07-06 16:41:55 +0200

    done

--- frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/App.jsx
+++ frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -20,6 +21,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>
---
+++ frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,3 @@
+export default function TabButton({children}) {
+  return <li><button>{children}</button></li>
+}
