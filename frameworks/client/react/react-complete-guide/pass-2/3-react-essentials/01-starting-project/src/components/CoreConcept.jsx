fe1d4b0b5b79d8b4dfb18ed526647fa8d7492c17
Piotr Nowak <p.nowak2@gmail.com>
2024-07-06 16:27:33 +0200

    done

--- frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/components/CoreConcept.jsx
+++ frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/components/CoreConcept.jsx
@@ -1,8 +1,11 @@
+import './CoreConcept.css';
 
-export default function CoreConcept({image, title, description}) {
-  return <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
