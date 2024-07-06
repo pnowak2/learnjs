80e49ca09044b818ff3ca8757608c6a1afb0185c
Piotr Nowak <p.nowak2@gmail.com>
2024-07-06 10:07:25 +0200

    done

--- frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/App.jsx
+++ frameworks/client/react/react-complete-guide/pass-2/3-react-essentials/01-starting-project/src/App.jsx
@@ -1,26 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(2)];
-
-  return (
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
+import Header from './components/Header';
 
 function CoreConcept({image, title, description}) {
   return <li>
