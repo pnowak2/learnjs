3594f88eb20e12ea9964861fd2f09ab4098963f5
Piotr Nowak <p.nowak2@gmail.com>
2024-07-12 22:46:51 +0200

    done

--- frameworks/client/react/react-complete-guide/pass-2/4-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ frameworks/client/react/react-complete-guide/pass-2/4-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,7 +1,19 @@
+import Player from "./components/Player.jsx"
 
 function App() {
   return (
-    <p>Coming soon...</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+
+        Game board
+      </div>
+
+      Log
+    </main>
   )
 }
 
