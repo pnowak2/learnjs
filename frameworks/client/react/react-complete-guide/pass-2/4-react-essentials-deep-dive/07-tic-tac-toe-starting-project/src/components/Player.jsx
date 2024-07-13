b028ec84ae7f40342ccd4f5165bdcaf7016f9f8e
Piotr Nowak <p.nowak2@gmail.com>
2024-07-13 08:51:55 +0200

    done

--- frameworks/client/react/react-complete-guide/pass-2/4-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ frameworks/client/react/react-complete-guide/pass-2/4-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,13 +4,15 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = 'Edit';
 
   if(isEditing) {
-    playerName = <input type="text" required />
+    playerName = <input type="text" required value={name} />
+    btnCaption='Save';
   }
 
   return (
@@ -19,7 +21,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
