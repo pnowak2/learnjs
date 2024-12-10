cc58751a03ff3d6ea154d7f8ded3b57920808c74
Piotr Nowak <p.nowak2@gmail.com>
2024-12-10 09:06:13 +0100

    done

--- frameworks/client/stencil.js/web-components-stenciljs-udemy/2024/3-basics/tooltip.js
+++ frameworks/client/stencil.js/web-components-stenciljs-udemy/2024/3-basics/tooltip.js
@@ -1,16 +1,44 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+
+    this._tooltipContainer = null;
+    this._tooltipText = 'not defined yet';
   }
 
   connectedCallback() {
+    if(this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+
     const tooltipIcon = document.createElement('span');
+
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+
     tooltipIcon.textContent = ' (?)';
     this.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   disconnectedCallback() {
     console.log('disconnected');
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = this._tooltipText;
+
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
   }
 
   attributeChangedCallback() {
