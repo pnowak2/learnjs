bdbb583a05db3a4057f6adcf2193ba5be306b8d5
Piotr Nowak <p.nowak2@gmail.com>
2025-02-13 15:35:18 +0100

    done

---
+++ frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html', 
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
