afe3f5f538d1d3b5e4ab1c4b68e09399de674208
Piotr Nowak <p.nowak2@gmail.com>
2025-02-13 15:48:40 +0100

    done

--- frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/app.component.ts
+++ frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
