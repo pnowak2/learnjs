64d51fbc4263659d97b8947a031966f15cb4149c
Piotr Nowak <p.nowak2@gmail.com>
2025-02-25 15:03:54 +0100

    done

--- frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/user/user.component.ts
+++ frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
