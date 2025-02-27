5a48a2b78a197d2ebd97fbef4782c1b36b5a3acd
Piotr Nowak <p.nowak2@gmail.com>
2025-02-27 15:12:27 +0100

    done

--- frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/app.component.ts
+++ frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
--- frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/user/user.component.ts
+++ frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,18 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  get imagePath(): string {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-
-  get avatar(): string {
-    return this.selectedUser.name;
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]
   }
 }
