987c35bd0730c9ca24322c887cfc804c7bee6c4f
Piotr Nowak <p.nowak2@gmail.com>
2025-03-04 15:54:48 +0100

    done

--- frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/user/user.component.ts
+++ frameworks/client/angular/angular-udemy/2-angular-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +7,19 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar()
+  })
+
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar();
+  // }
 
   onSelectUser() {
   }
