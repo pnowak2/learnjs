f250def60a67a7b39e1faf3c4ea2a69f236d42fd
pnowak2 <p.nowak2@gmail.com>
2025-05-24 20:20:37 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 5-2025/flappy/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 5-2025/flappy/src/index.js
@@ -3,7 +3,22 @@ import Phaser from 'phaser';
 let config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600
+  height: 600,
+  physics: {
+    default: 'arcade'
+  },
+  scene: {
+    preload,
+    create
+  }
 };
+
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+}
+
+function create() {
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+}
 
 new Phaser.Game(config);
