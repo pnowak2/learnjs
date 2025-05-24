4420d0de564ef5e6b0ee5bc28cabdbd92ead1933
pnowak2 <p.nowak2@gmail.com>
2025-05-24 21:00:59 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 5-2025/flappy/src/index.js
+++ frameworks/client/phaser/phaser - the complete guide - udemy 5-2025/flappy/src/index.js
@@ -22,7 +22,9 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  bird = this.add.sprite(config.width / 10, config.height / 2 , 'bird').setOrigin(0.5, 0.5);
+  bird = this.physics.add.sprite(config.width / 10, config.height / 2 , 'bird').setOrigin(0.5, 0.5);
+  bird.body.gravity.y = 200;
+  bird.body.velocity.x = 200;
 }
 
 new Phaser.Game(config);
