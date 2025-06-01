7ee87170d6d81a4cabb4ded66baa75cea311c9b7
pnowak2 <p.nowak2@gmail.com>
2025-06-01 09:10:50 +0200

    done

--- frameworks/client/phaser/phaser - the complete guide - udemy 5-2025/dino/src/entities/player.ts
+++ frameworks/client/phaser/phaser - the complete guide - udemy 5-2025/dino/src/entities/player.ts
@@ -27,13 +27,20 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.registerAnimations();
   }
 
-  update() {
+  update(x: any, y: any) {
     const { space } = this.cursors;
     const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space);
     const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor();
 
     if (isSpaceJustDown && onFloor) {
       this.setVelocityY(-1600);
+    }
+
+    if(this.body.deltaAbsY() > 0 ) {
+      this.anims.stop();
+      this.setTexture('dino-run', 0);
+    } else {
+      this.playRunAnimation();
     }
   }
 
