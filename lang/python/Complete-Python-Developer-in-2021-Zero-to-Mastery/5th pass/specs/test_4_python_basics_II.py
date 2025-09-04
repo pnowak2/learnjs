8499ca2579767d5f08866335f6377717df45cfdc
pnowak2 <p.nowak2@gmail.com>
2025-09-04 13:46:13 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_4_python_basics_II.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_4_python_basics_II.py
@@ -2,5 +2,51 @@ import math
 
 class TestPythonBasicsII:
     class TestConditionalLogic:
-        def test(self):
-            pass
+        class TestIf:
+            def test_if_else(self):
+                is_old = True
+                is_licenced = True
+
+                if is_old:
+                    pass
+                else:
+                    assert(False)
+
+                is_old = False
+
+                if is_old:
+                    assert(False)
+                else:
+                    pass
+
+            def test_elif(self):
+                is_old = True
+                is_licenced = True
+
+                if is_old:
+                    pass
+                elif is_licenced:
+                    pass
+                else:
+                    assert(False)
+
+            def test_and_or(self):
+                is_old = True
+                is_licenced = True
+
+                if is_old and is_licenced:
+                    pass
+                else:
+                    assert(False)
+
+                is_licenced = False
+
+                if is_old or is_licenced:
+                    pass
+                else:
+                    assert(False)
+
+            def test_or_fallback(self):
+                result = False or 'default'
+
+                assert(result == 'default')
