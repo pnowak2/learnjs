d54b013cba1b4a33945dec455252139f82930575
pnowak2 <p.nowak2@gmail.com>
2025-09-07 21:08:04 +0200

    done

--- tools/chatgpt-and-generative-ai-the-complete-guide/section-18/openai-api/app.py
+++ tools/chatgpt-and-generative-ai-the-complete-guide/section-18/openai-api/app.py
@@ -1,17 +1,21 @@
+import os
 import requests
 
+api_key = os.environ.get('OPENAI_API_KEY')
 api_url = "https://api.openai.com/v1/responses"
 headers = {
     "Content-Type": "application/json",
-    # "Authorization": "Bearer KEY",
+    "Authorization": f"Bearer {api_key}",
 }
 
-data = '''{
-    "model": "gpt-5",
+if not api_key:
+    raise RuntimeError("Missing OPENAI_API_KEY env variable")
+
+body ={
+    "model": "gpt-4o-mini",
     "input": "Write a one-sentence bedtime story about a unicorn."
 }
-'''
 
-res = requests.post(api_url, headers=headers, data=data)
+res = requests.post(api_url, headers=headers, data=body)
 
 print(res)
