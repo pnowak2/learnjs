678131671cfc19ae66652d63ce25d29c33c5aee0
pnowak2 <p.nowak2@gmail.com>
2025-09-15 16:00:33 +0200

    done

--- tools/chatgpt-and-generative-ai-the-complete-guide/section-21/starting-project/src/components/Header/Header.jsx
+++ tools/chatgpt-and-generative-ai-the-complete-guide/section-21/starting-project/src/components/Header/Header.jsx
@@ -3,13 +3,20 @@ import './Header.css';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+} 
+
 export default function Header() {
+  const randomIndex = genRandomInt(reactDescriptions.length);
+  const reactDescription = reactDescriptions[randomIndex];
+
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        React concepts you will need for almost any app you are going to build!
+        {reactDescription} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
