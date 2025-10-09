f93fe7b1ecc34204648025401c952d1f31d2b51b
pnowak2 <p.nowak2@gmail.com>
2025-10-09 16:25:02 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_8_decorators.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_8_decorators.py
@@ -1,3 +1,28 @@
 class TestDecorators:
-    def test(self):
-        pass
+    def test_function_pointers(self):
+        def hello():
+            return 'hello'
+
+        greet = hello
+        del hello
+        
+        assert(greet() == 'hello')
+        
+    def test_passing_functions(self):
+        def hello(fn):
+            return f'calling fn: {fn()}'
+
+        def greet():
+            return 'hello fellow friend'
+        
+        assert(hello(greet) == 'calling fn: hello fellow friend')
+        
+    def test_higher_order_functions(self):
+        def make_fn():
+            def internal_fn():
+                return 5
+
+            return internal_fn
+
+        assert(make_fn()() == 5)
+        
