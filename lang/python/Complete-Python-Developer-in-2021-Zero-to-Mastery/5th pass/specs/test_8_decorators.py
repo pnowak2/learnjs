bdca905d46d7bc36198907302b384bf46aec37c0
pnowak2 <p.nowak2@gmail.com>
2025-10-16 15:24:25 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_8_decorators.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_8_decorators.py
@@ -5,18 +5,18 @@ class TestDecorators:
 
         greet = hello
         del hello
-        
-        assert(greet() == 'hello')
-        
+
+        assert (greet() == 'hello')
+
     def test_passing_functions(self):
         def hello(fn):
             return f'calling fn: {fn()}'
 
         def greet():
             return 'hello fellow friend'
-        
-        assert(hello(greet) == 'calling fn: hello fellow friend')
-        
+
+        assert (hello(greet) == 'calling fn: hello fellow friend')
+
     def test_higher_order_functions(self):
         def make_fn(param):
             def internal_fn(first, *args, **kwargs):
@@ -24,5 +24,35 @@ class TestDecorators:
 
             return internal_fn
 
-        assert(make_fn(5)(2) == 10)
-        
+        assert (make_fn(5)(2) == 10)
+
+    def test_first_decorator(self):
+        def my_decorator(fn):
+            def wrapper_fn(*args):
+                return fn(*args)
+
+            return wrapper_fn
+
+        @my_decorator
+        def hello(name):
+            return f'hello {name}'
+
+        result = hello('piotr')
+        assert(result == 'hello piotr')
+
+    def test_decorator_with_params(self):
+        def my_decorator(arg):
+            def actual_decorator(fn):
+                def wrapper_fn(*args):
+                    return fn(*args) + arg 
+
+                return wrapper_fn
+
+            return actual_decorator
+
+        @my_decorator('!')
+        def hello(name):
+            return f'hello {name}'
+
+        result = hello('piotr')
+        assert(result == 'hello piotr!')
