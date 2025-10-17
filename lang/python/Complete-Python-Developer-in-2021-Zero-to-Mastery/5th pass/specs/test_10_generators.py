d5c318e5aa8a21fad9e699f99e2e13394d6d5b03
pnowak2 <p.nowak2@gmail.com>
2025-10-17 16:16:27 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_10_generators.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_10_generators.py
@@ -1,4 +1,44 @@
 
 class TestGenerators:
-    def test(self):
-      pass
+    def test_sequential_concept(self):
+      def make_list(n):
+         result = []
+         for i in range(n):
+            result.append(i)
+
+         return result
+
+      result = make_list(5)
+
+      assert(result == [0, 1, 2, 3, 4])
+      # taking up space in memory
+      # range is a generator, does not fill memory, just gives sequential values
+
+    def test_generator_with_yield_and_next(self):
+      def generator_fn():
+            yield 0 # pause function, give value and wait what to do next
+            yield 1 # pause function, give value and wait what to do next
+            yield 2 # pause function, give value and wait what to do next
+
+      gen = generator_fn()
+
+      try:
+        assert(next(gen) == 0)
+        assert(next(gen) == 1)
+        assert(next(gen) == 2)
+
+        next(gen)
+
+        assert False
+
+      except StopIteration as err:
+         assert True
+
+    def test_iterate_with_generator(self):
+      def generator_fn(n):
+         for i in range(n):
+            yield i
+
+      result = [item for item in generator_fn(3)]
+
+      assert(result == [0, 1, 2])
