5f5d0b03d329cbdae49deb4477cf4f190d51d3e6
pnowak2 <p.nowak2@gmail.com>
2025-10-17 16:41:37 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_10_generators.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_10_generators.py
@@ -42,3 +42,42 @@ class TestGenerators:
       result = [item for item in generator_fn(3)]
 
       assert(result == [0, 1, 2])
+
+    def test_iter(self):
+       itr = iter([1, 2, 3])
+
+       assert(next(itr) == 1)
+       assert(next(itr) == 2)
+       assert(next(itr) == 3)
+
+    def test_custom_generator(self):
+      class MyRange:
+        def __init__(self, frm ,to):
+           self.frm = frm
+           self.to = to
+
+           self.current = frm
+
+        def __iter__(self):
+           return self
+
+        def __next__(self):
+           if(self.current < self.to):
+              item = self.current
+              self.current += 1
+
+              return item
+           else:
+              raise StopIteration
+
+      myRange = MyRange(1, 3)
+      assert(next(myRange) == 1)
+      assert(next(myRange) == 2)
+
+      try:
+        assert(next(myRange) == 3)
+        assert False
+      except StopIteration:
+        assert True
+
+         
