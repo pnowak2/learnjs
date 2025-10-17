1e6471a00d050b15ede7eb23a2c16c2a6a2fc369
pnowak2 <p.nowak2@gmail.com>
2025-10-17 18:55:33 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_11_modules.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_11_modules.py
@@ -1,9 +1,11 @@
 import utility.math as math
+from utility import math as m
 from utility.math import sum, divide
 
 class TestModules:
   def test_importing_module_from_package(self):
     assert(math.divide(6, 3) == 2)
     assert(math.sum(6, 3) == 9)
+    assert(m.sum(6, 3) == 9)
     assert(sum(6, 3) == 9)
     assert(divide(9, 3) == 3)
