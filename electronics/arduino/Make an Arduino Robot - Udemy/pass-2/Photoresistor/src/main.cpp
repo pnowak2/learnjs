01a4c1131dcaac2b4ae72d0ed7c42fdd6b9784ae
pnowak2 <p.nowak2@gmail.com>
2025-10-18 18:41:07 +0200

    done

--- electronics/arduino/Make an Arduino Robot - Udemy/pass-2/Photoresistor/src/main.cpp
+++ electronics/arduino/Make an Arduino Robot - Udemy/pass-2/Photoresistor/src/main.cpp
@@ -6,26 +6,18 @@
  */
 #include "Arduino.h"
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+int sensorPin = A2;
+int sensorValue = 0;
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(10);
 }
