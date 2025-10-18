430033edc82907724036614ce62352723636493f
pnowak2 <p.nowak2@gmail.com>
2025-10-18 05:46:33 +0200

    done

--- tools/docker/docker-kubernetes-udemy/pass-2/4-networking/networks-starting-setup/README.md
+++ tools/docker/docker-kubernetes-udemy/pass-2/4-networking/networks-starting-setup/README.md
@@ -2,3 +2,6 @@ docker build -t favorites-node .
 docker run --name favorites -d --rm -p 3000:3000 favorites-node
 
 host.docker.internal - points to host machine IP address
+
+docker run mongo --name mongodb mongo
+docker inspect mongodb // to get ip address of this container
--- tools/docker/docker-kubernetes-udemy/pass-2/4-networking/networks-starting-setup/app.js
+++ tools/docker/docker-kubernetes-udemy/pass-2/4-networking/networks-starting-setup/app.js
@@ -69,8 +69,20 @@ app.get('/people', async (req, res) => {
 
 app.listen(3000);
 
+// mongoose.connect(
+//   'mongodb://host.docker.internal:27017/swfavorites',
+//   { useNewUrlParser: true },
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       app.listen(3000);
+//     }
+//   }
+// );
+
 mongoose.connect(
-  'mongodb://host.docker.internal:27017/swfavorites',
+  'mongodb://172.17.0.2:27017/swfavorites',
   { useNewUrlParser: true },
   (err) => {
     if (err) {
