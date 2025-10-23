a770c1846e812b0abbb0ef820c9af6ac0446350d
pnowak2 <p.nowak2@gmail.com>
2025-10-23 05:57:11 +0200

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_13_fileio.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_13_fileio.py
@@ -1,3 +1,31 @@
 class TestFileIO:
-  def test(self):
-    pass
+  def test_open(self):
+    my_file = open(f"specs/files/readme.md")
+    assert(my_file.read() == 'hello world')
+    # cursor is at the end of the line, as line has been read
+    assert(my_file.read() == '')
+
+    my_file.close()
+
+  def test_seek(self):
+    my_file = open(f"specs/files/readme.md")
+    assert(my_file.read() == 'hello world')
+
+    # cursor is at the end of the line
+    assert(my_file.read() == '')
+
+    my_file.seek(0)
+
+    # move cursor to the beginning
+    assert(my_file.read() == 'hello world')
+
+    my_file.close()
+
+  def test_readline(self):
+    my_file = open(f"specs/files/multiline.txt")
+    assert(my_file.readline() == 'line 1\n')
+    assert(my_file.readline() == 'line 2\n')
+    assert(my_file.readline() == 'line 3')
+    assert(my_file.readline() == '')
+
+    my_file.close()
