3fc7c12e6bb8511517b61d0c6f406f3e4e772b02
pnowak2 <p.nowak2@gmail.com>
2025-10-27 14:28:50 +0100

    done

--- electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/2-basics-and-basic-io/2.6-reading-analog-inputs.py
+++ electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/2-basics-and-basic-io/2.6-reading-analog-inputs.py
@@ -2,6 +2,7 @@ from machine import Pin, ADC
 from time import sleep
 
 pot = ADC(Pin(26))
+led = Pin(19, Pin.OUT)
 
 conversion_factor = 3.3 / (2**16 - 1)
 
@@ -9,5 +10,10 @@ while True:
   raw_data = pot.read_u16() # 16 bit precision, 2^16-1 levels
   voltage =  raw_data * conversion_factor
   print(f'raw: {raw_data}, voltage: {voltage}V')
+
+  if(raw_data > 600):
+    led.on()
+  else:
+    led.off()
 
   sleep(0.1)
