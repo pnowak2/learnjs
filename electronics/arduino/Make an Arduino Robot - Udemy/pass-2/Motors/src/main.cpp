c5beb2ae5a6bcaa6767ce157b367be08ce65af1a
pnowak2 <p.nowak2@gmail.com>
2025-10-27 13:44:48 +0100

    done

--- electronics/arduino/Make an Arduino Robot - Udemy/pass-2/Motors/src/main.cpp
+++ electronics/arduino/Make an Arduino Robot - Udemy/pass-2/Motors/src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/**
+ * range for cotinous motor servo is 0-180
+ * 90 is neutral, below is one direction, above another direction
+ * speed regulated by all values in between
+ */
+
+ /**
+  * range for non-continous servo motor is also 0-180
+  * but this time min and max values denote angle how shaft should turn
+  */
+
+const int RForward = 120; // the speed of the servo, max. speed is 180
+const int RBackward = 60;
+
+const int LForward = 120;
+const int LBackward = 60;
+
+const int RNeutral = 90; // centered position
+const int LNeutral = 90;
+
+Servo leftMotor;
+Servo rightMotor;
+Servo panMotor;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  rightMotor.attach(11); // pin 11
+  leftMotor.attach(10); // pin 10
+  panMotor.attach(6); // pin 6
+
+  panMotor.write(90); // set to center position
+  leftMotor.write(LNeutral);
+  rightMotor.write(RNeutral);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  panMotor.write(0); // full right
+  leftMotor.write(LForward);
+  rightMotor.write(RForward);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+
+  panMotor.write(180); // full left
+  leftMotor.write(LBackward);
+  rightMotor.write(RBackward);
+
+  delay(1000);
+
+  panMotor.write(90); // center
+  leftMotor.write(LForward);
+  rightMotor.write(RBackward);
+
+  delay(1000);
 }
