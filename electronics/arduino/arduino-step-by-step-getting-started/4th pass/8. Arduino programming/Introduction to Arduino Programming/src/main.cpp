f41ad0f4b16b8418e6e5e0dc59662f67508faa45
pnowak2 <p.nowak2@gmail.com>
2025-10-28 10:26:40 +0100

    done

--- electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/Introduction to Arduino Programming/src/main.cpp
+++ electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/Introduction to Arduino Programming/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include <foo.cpp>
 
+int sum(int a, int b) {
+  return a + b;
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello World");
   Serial.println(domeSomeCalc());
+  Serial.println(sum(7, 9));
 }
 
 void loop() {
