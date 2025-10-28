87ace47a54bf2e1b667d4f39cb8bd313d32c8917
pnowak2 <p.nowak2@gmail.com>
2025-10-28 06:16:02 +0100

    done

---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/15-unit-testing/test.py
@@ -0,0 +1,11 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+  def test_do_stuff(self):
+    test_param = 10
+    result = main.do_stuff(test_param)
+
+    self.assertEqual(result, 100)
+
+unittest.main()
