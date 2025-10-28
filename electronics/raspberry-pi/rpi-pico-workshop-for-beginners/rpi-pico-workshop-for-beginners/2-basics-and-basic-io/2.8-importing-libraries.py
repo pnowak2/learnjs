f7c3e8c0138031ad24d5afa8414f22ca50fa174e
pnowak2 <p.nowak2@gmail.com>
2025-10-28 15:29:07 +0100

    done

--- electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/2-basics-and-basic-io/2.8-importing-libraries.py
+++ electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/2-basics-and-basic-io/2.8-importing-libraries.py
@@ -1,7 +1,16 @@
 from machine import Pin
 import time
-import math
-import bluetooth
+from servo import Servo
+
+my_servo = Servo(pin_id=17)
 
 while True:
-  pass
+  my_servo.write(0)
+  print('0')
+  time.sleep(1)
+  my_servo.write(90)
+  print('90')
+  time.sleep(1)
+  my_servo.write(180)
+  print('180')
+  time.sleep(1)
