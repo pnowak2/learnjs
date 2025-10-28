be69b3200d1da5c083d30a5dfa78f84e44e125f0
pnowak2 <p.nowak2@gmail.com>
2025-10-28 10:12:02 +0100

    done

--- electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/Introduction to Arduino Programming/src/main.cpp
+++ electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/Introduction to Arduino Programming/src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello World");
+  Serial.print("Hello Again");
 }
 
 void loop() {
+  Serial.println(millis());
+  Serial.println(micros());
+  delay(1000);
 }
