ebfe717c4b49b3150229188f877e6a74cb7106cc
pnowak2 <p.nowak2@gmail.com>
2025-10-29 14:53:07 +0100

    done

--- electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/Button/src/main.cpp
+++ electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/Button/src/main.cpp
@@ -3,11 +3,18 @@
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   int value = digitalRead(2);
   Serial.println(value);
+
+  if(value == 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
   delay(500);
 }
