d3b61ff54d7e25b1c1eb72692f980135cfb64870
pnowak2 <p.nowak2@gmail.com>
2025-10-29 07:04:06 +0100

    done

---
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/15-unit-testing/README.md
@@ -0,0 +1,4 @@
+# will find and run all unit tests
+python3 -m unittest -v 
+
+-v for verbose
--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/15-unit-testing/test.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/15-unit-testing/test.py
@@ -2,18 +2,22 @@ import unittest
 import main
 
 class TestMain(unittest.TestCase):
+  def setUp(self):
+    print('about to test')
+
   def test_do_stuff(self):
+    '''Some description for this test'''
     test_param = 10
     result = main.do_stuff(test_param)
 
     self.assertEqual(result, 100)
 
-  def test_other_stuff(self):
+  def test_string(self):
     test_param = 'test'
     result = main.do_stuff(test_param)
     self.assertIsInstance(result, ValueError)
 
-  def test_sth_else(self):
+  def test_none(self):
     test_param = None
     result = main.do_stuff(test_param)
     self.assertEqual(result, 'please enter a number')
@@ -22,6 +26,14 @@ class TestMain(unittest.TestCase):
     test_param = ''
     result = main.do_stuff(test_param)
     self.assertEqual(result, 'please enter a number')
+
+  def test_with_zero(self):
+    test_param = 0
+    result = main.do_stuff(test_param)
+    self.assertEqual(result, 'please enter a number')
+
+  def tearDown(self):
+    print('cleaning up')
 
 if __name__ == '__main__':
   unittest.main()
