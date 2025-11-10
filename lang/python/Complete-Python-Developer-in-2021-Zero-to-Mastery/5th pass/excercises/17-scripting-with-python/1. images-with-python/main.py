a3607caa0bc5236fd11b2ed402188c2d675ae1c9
pnowak2 <p.nowak2@gmail.com>
2025-11-10 06:21:09 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/17-scripting-with-python/1. images-with-python/main.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/17-scripting-with-python/1. images-with-python/main.py
@@ -1,1 +1,44 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+from os import path
+
+img = Image.open('./images/pikachu.jpg')
+
+print(dir(img))
+
+print(img.format)
+print(img.size)
+print(img.mode)
+
+# filtering
+
+blur_img = img.filter(ImageFilter.BLUR)
+blur_img.save(path.join("./output", "pikachu-blur.png"), "png")
+
+smooth_img = img.filter(ImageFilter.SMOOTH)
+smooth_img.save(path.join("./output", "pikachu-smooth.png"), "png")
+
+sharp_img = img.filter(ImageFilter.SHARPEN)
+sharp_img.save(path.join("./output", "pikachu-sharp.png"), "png")
+
+# formats
+
+grey_img = img.convert('L')
+grey_img.save(path.join("./output", "pikachu-grey.png"), "png")
+
+# shows image in default viewer
+# grey_img.show()
+
+# rotate
+
+rotated_img = sharp_img.rotate(180)
+rotated_img.save(path.join("./output", "rotated.png"), "png")
+
+# resize 
+
+resized_img = img.resize((300, 300))
+resized_img.save(path.join("./output", "resized.png"), "png")
+
+# crop
+
+crop_img = img.crop((100, 100, 200, 200))
+crop_img.save(path.join("./output", "crop.png"), "png")
