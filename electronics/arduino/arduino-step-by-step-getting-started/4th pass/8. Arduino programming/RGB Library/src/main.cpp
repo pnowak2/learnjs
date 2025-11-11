209a0181fa6a4fdbfb499bc1393b817a0a321065
pnowak2 <p.nowak2@gmail.com>
2025-11-11 13:19:28 +0100

    done

---
+++ electronics/arduino/arduino-step-by-step-getting-started/4th pass/8. Arduino programming/RGB Library/src/main.cpp
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// ALA library example: SimpleRgbLed
+//
+// Example to demonstrate how to create a color-fading animation for one RGB LED.
+//
+// Web page: http://yaab-arduino.blogspot.com/p/ala-example-simplergbled.html
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+#include <AlaLedRgb.h>
+
+#define REDPIN 3
+#define GREENPIN 5
+#define BLUEPIN 6
+
+AlaLedRgb rgbLed;
+
+void setup()
+{
+  rgbLed.initPWM(REDPIN, GREENPIN, BLUEPIN);                 // initialize output pins
+  rgbLed.setBrightness(0x66FF44);                            // calibrate white
+  rgbLed.setAnimation(ALA_FADECOLORSLOOP, 5000, alaPalRgb);  // set the animation
+}
+
+void loop()
+{
+  rgbLed.runAnimation();  // run the animation indefinitely
+}
+
