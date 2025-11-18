ad33177a2c56e088c1f71379fdcdf54ea9d39bb4
pnowak2 <p.nowak2@gmail.com>
2025-11-18 06:23:01 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/17-scripting-with-python/3.email/send-mail.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/17-scripting-with-python/3.email/send-mail.py
@@ -1,12 +1,16 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Piotrek'
 email['to'] = 'biuro@domi-soft.com'
 email['subject'] = 'Testing pythong email api'
 
-email.set_content('This is sample content')
+email.set_content(html.substitute({'name': 'AI Agent'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo()
