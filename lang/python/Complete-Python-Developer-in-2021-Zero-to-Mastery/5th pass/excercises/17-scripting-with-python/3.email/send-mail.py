a5b89e2e18292cac96c86415463f0be0291d2753
pnowak2 <p.nowak2@gmail.com>
2025-11-18 06:15:28 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/17-scripting-with-python/3.email/send-mail.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/17-scripting-with-python/3.email/send-mail.py
@@ -1,2 +1,18 @@
 import smtplib
 from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Piotrek'
+email['to'] = 'biuro@domi-soft.com'
+email['subject'] = 'Testing pythong email api'
+
+email.set_content('This is sample content')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('p.nowak2@gmail.com', 'avon vpfa dlyd wked')
+  smtp.send_message(email)
+
+  print('done')
+
