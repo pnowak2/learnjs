8ad3dd4aeeb67c4a4210352de7f9bf9754688435
pnowak2 <p.nowak2@gmail.com>
2025-11-21 07:06:35 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/18-scraping-data-with-python/scraper.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/18-scraping-data-with-python/scraper.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
-content = soup.find_all(class_='titleline')
+content = soup.find_all(class_='titleline', limit=2)
+
+print(content)
