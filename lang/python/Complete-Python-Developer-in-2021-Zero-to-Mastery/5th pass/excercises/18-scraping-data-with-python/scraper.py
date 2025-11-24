bfe86d52a0ab7edb3b9cc6c7d4f7ec1109787a9b
pnowak2 <p.nowak2@gmail.com>
2025-11-24 06:53:32 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/18-scraping-data-with-python/scraper.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/18-scraping-data-with-python/scraper.py
@@ -4,6 +4,22 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
-content = soup.find_all(class_='titleline', limit=2)
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
 
-print(content)
+def create_custom_hn(links, subtext):
+  hn = []
+
+  for idx, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[idx].select('.score')
+
+    if len(vote):
+      points = int(vote[0].getText().replace(' points', ''))
+      hn.append({'title': title, 'link': href, 'points': points})
+
+  return hn
+
+result = create_custom_hn(links, subtext)
+print(result)
