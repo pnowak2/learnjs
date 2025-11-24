d57ca25f37cffd4d6f48a9418a1f1635ffd4c511
pnowak2 <p.nowak2@gmail.com>
2025-11-24 13:16:50 +0100

    done

--- electronics/arduino/arduino-step-by-step-getting-started/4th pass/10-Measuring light and color/IR Remote Controller/src/main.cpp
+++ electronics/arduino/arduino-step-by-step-getting-started/4th pass/10-Measuring light and color/IR Remote Controller/src/main.cpp
@@ -12,7 +12,7 @@ void setup()
   pinMode(EN_PIN, OUTPUT);
   digitalWrite(EN_PIN, LOW);
 
-  // IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
   IrSender.begin(TX_PIN, ENABLE_LED_FEEDBACK);
 }
 
@@ -26,9 +26,53 @@ void decode()
     IrReceiver.resume();
   }
   /*
-   * kod on/off pilota do lampki biurowej
+   * KOD PILOTA DO LAMPKI BIUROWEJ
+   *
+   * ON / OFF
    * Protocol=NEC Address=0x0, Command=0x1, Raw-Data=0xFE01FF00, 32 bits, LSB first, Gap=3276750us, Duration=68300us
    * Send with: IrSender.sendNEC(0x0, 0x1, <numberOfRepeats>);
+   * 
+   * LIGHTER 
+   * Protocol=NEC Address=0x0, Command=0x9, Raw-Data=0xF609FF00, 32 bits, LSB first, Gap=3276750us, Duration=68300us
+   * Send with: IrSender.sendNEC(0x0, 0x9, <numberOfRepeats>);
+   * 
+   * DIMMER 
+   * Protocol=NEC Address=0x0, Command=0x11, Raw-Data=0xEE11FF00, 32 bits, LSB first, Gap=3276750us, Duration=68250us
+   * Send with: IrSender.sendNEC(0x0, 0x11, <numberOfRepeats>);
+   */
+
+  /*
+   * KOD WENTYLATORA BRANDSON
+   *
+   * ON / OFF
+   * Protocol=UNKNOWN Hash=0xDA0AE878 24 bits (incl. gap and start) received
+   * 
+   * DISPLAY
+   * Protocol=UNKNOWN Hash=0xF40BF22B 24 bits (incl. gap and start) received
+   */
+
+  /*
+   * KOD DO TEALIGHTS 
+   *
+   * ON 
+   * Protocol=NEC Address=0x93CB, Command=0x50, Raw-Data=0xAF5093CB, 32 bits, LSB first, Gap=1877700us, Duration=68850us
+   * end with: IrSender.sendNEC(0x93CB, 0x50, <numberOfRepeats>);
+   * 
+   * OFF 
+   * Protocol=NEC Address=0x93CB, Command=0x5F, Raw-Data=0xA05F93CB, 32 bits, LSB first, Gap=594700us, Duration=68850us
+   * Send with: IrSender.sendNEC(0x93CB, 0x5F, <numberOfRepeats>);
+   * 
+   * FLICKER
+   * Protocol=NEC Address=0x93CB, Command=0x3B, Raw-Data=0xC43B93CB, 32 bits, LSB first, Gap=763350us, Duration=68800us
+   * end with: IrSender.sendNEC(0x93CB, 0x3B, <numberOfRepeats>);
+   * 
+   * TIMER 4h
+   * Protocol=NEC Address=0x93CB, Command=0x2F, Raw-Data=0xD02F93CB, 32 bits, LSB first, Gap=3276750us, Duration=68800us
+   * Send with: IrSender.sendNEC(0x93CB, 0x2F, <numberOfRepeats>);
+   * 
+   * TIMER 6h
+   * Protocol=NEC Address=0x93CB, Command=0x2A, Raw-Data=0xD52A93CB, 32 bits, LSB first, Gap=1362250us, Duration=68800us
+   * Send with: IrSender.sendNEC(0x93CB, 0x2A, <numberOfRepeats>);
    */
 }
 
@@ -40,6 +84,6 @@ void sendOnOff()
 
 void loop()
 {
-  // decode();
-  sendOnOff();
+  decode();
+  // sendOnOff();
 }
