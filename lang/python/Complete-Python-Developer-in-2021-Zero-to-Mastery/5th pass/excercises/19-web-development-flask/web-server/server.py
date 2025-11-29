dceefca0cb3074f22f25192624ce472e358a2933
pnowak2 <p.nowak2@gmail.com>
2025-11-29 06:14:22 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/19-web-development-flask/web-server/server.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/19-web-development-flask/web-server/server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('index.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+# @app.route('/favicon.ico')
+# def favicon():
+#   return 'Blog'
+
+@app.route('/blog/2025/dogs')
+def blog2():
+  return 'Dogs'
 
 if __name__ == '__main__':
   app.run(debug=True)
