68a45cbb60a207b1440f217fc05150bd1df3b186
pnowak2 <p.nowak2@gmail.com>
2025-11-29 06:32:23 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/19-web-development-flask/web-server/server.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/excercises/19-web-development-flask/web-server/server.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
+  print(url_for('static', filename='favicon.png'))
   return render_template('index.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/about/<username>')
+def user_profile(username = None):
+  ctx = {
+    'user': username
+  }
+
+  return render_template('about.html', **ctx)
+
+@app.route('/blog/<username>/<int:post_id>')
+def blog(username = None, post_id = None):
+  return render_template('blog.html', user=username, post_id=post_id)
 
 # @app.route('/favicon.ico')
 # def favicon():
