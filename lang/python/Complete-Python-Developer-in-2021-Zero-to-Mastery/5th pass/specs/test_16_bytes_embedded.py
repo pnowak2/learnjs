862181bd026e9231fc6754b651164a057b2b9c52
pnowak2 <p.nowak2@gmail.com>
2025-12-03 18:25:09 +0100

    done

--- lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_16_bytes_embedded.py
+++ lang/python/Complete-Python-Developer-in-2021-Zero-to-Mastery/5th pass/specs/test_16_bytes_embedded.py
@@ -1,3 +1,23 @@
 class TestBytesEmbeddedSystems:
-  def test(self):
-    pass
+  def test_bin(self):
+    assert(bin(7) == '0b111')
+    assert(bin(15) == '0b1111')
+
+  def test_hex(self):
+    assert(hex(15) == '0xf')
+    assert(hex(255) == '0xff')
+
+  def test_byte_literal(self):
+    assert(len(b'A') == 1)
+    assert(len(b'ABC') == 3)
+
+    assert(b'ABC'[0] == 65)
+    assert(b'ABC'[1] == 66)
+    assert(b'ABC'[2] == 67)
+
+    assert(b'\xff'[0] == 255)
+    assert(b'\xff'.hex() == 'ff')
+
+  def test_bytes(self):
+    b = bytes(4)
+    assert(b[0] == 0)
