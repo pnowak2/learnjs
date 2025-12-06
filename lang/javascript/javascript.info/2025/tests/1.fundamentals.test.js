1b87e3c28f65dbd6192c4454cd16874885130728
pnowak2 <p.nowak2@gmail.com>
2025-12-06 23:04:02 +0100

    done

--- lang/javascript/javascript.info/2025/tests/1.fundamentals.test.js
+++ lang/javascript/javascript.info/2025/tests/1.fundamentals.test.js
@@ -1,7 +1,9 @@
 import { describe, it, expect } from 'vitest';
+import { add } from '../src/2.fundamentals/add';
 
 describe('Fundamentals', () => {
   it('should..', () => {
     expect(2 + 3).toBe(5);
+    expect(add(2, 7)).toEqual(9);
   });
 });
