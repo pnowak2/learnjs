7687c9349f5a6c9270321d985940589b9d685ebf
pnowak2 <p.nowak2@gmail.com>
2025-12-08 09:06:16 +0100

    done

--- lang/javascript/javascript.info/2025/tests/1.fundamentals.test.js
+++ lang/javascript/javascript.info/2025/tests/1.fundamentals.test.js
@@ -2,8 +2,45 @@ import { describe, it, expect } from 'vitest';
 import { add } from '../src/2.fundamentals/add';
 
 describe('Fundamentals', () => {
-  it('should..', () => {
-    expect(2 + 3).toBe(5);
-    expect(add(2, 7)).toEqual(9);
+  describe('Variables', () => {
+    it('should declare variables', () => {
+      let msg = 'hello';
+      expect(msg).toEqual('hello');
+    });
+
+    it('should declare multiline vars', () => {
+      let user = 'piotr',
+        age = 45,
+        message = 'hello';
+      expect(user).toEqual('piotr');
+      expect(age).toEqual(45);
+      expect(message).toEqual('hello');
+    });
+
+    it('should use const', () => {
+      const username = 'piotr';
+      // username = 'buba'; not allowed
+
+      expect(username).toEqual('piotr');
+    });
+  });
+
+  describe('Data types', () => {
+    describe('Number', () => {
+      it('should declare use Number', () => {
+        let n = 123;
+        n = 12.345;
+
+        expect(n).toEqual(12.345);
+      });
+
+      it('should use Infinity', () => {
+        expect(2/0).toBe(Infinity);
+      });
+
+      it('should use Nan', () => {
+        expect(2*'abc').toBe(NaN);
+      });
+    })
   });
 });
