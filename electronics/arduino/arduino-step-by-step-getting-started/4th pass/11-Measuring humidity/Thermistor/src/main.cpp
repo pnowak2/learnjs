f2f4556de5f9a0cdd396e2968bd50abf807cb56b
pnowak2 <p.nowak2@gmail.com>
2025-12-09 05:26:51 +0100

    done

--- electronics/arduino/arduino-step-by-step-getting-started/4th pass/11-Measuring humidity/Thermistor/src/main.cpp
+++ electronics/arduino/arduino-step-by-step-getting-started/4th pass/11-Measuring humidity/Thermistor/src/main.cpp
@@ -1,16 +1,41 @@
-#include "Arduino.h"
+#include "thermistor.h"
+#include "HardwareSerial.h"
 
-#define SENSOR_PIN A2
 
-void setup() {
+// Analog pin used to read the NTC
+#define NTC_PIN               A2
+
+// Thermistor object
+THERMISTOR thermistor(NTC_PIN,        // Analog pin
+                      10300,          // Nominal resistance at 25 ºC
+                      3950,           // thermistor's beta coefficient
+                      4660);         // Value of the series resistor
+
+// Global temperature reading
+uint16_t temp;
+
+/**
+ * setup
+ *
+ * Arduino setup function
+ */
+void setup()
+{
   Serial.begin(9600);
-  pinMode(SENSOR_PIN, INPUT);
 }
 
-void loop() {
-  int rawValue = analogRead(SENSOR_PIN);
-  float reading = rawValue * (1.0/1023);
+/**
+ * loop
+ *
+ * Arduino main loop
+ */
+void loop()
+{
+  temp = thermistor.read();   // Read temperature
 
-  Serial.println(reading);
-  delay(500);
+  Serial.print("Temp in 1/10 ºC : ");
+  Serial.println(temp);
+
+  delay(5000);
 }
+
