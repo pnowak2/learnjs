21a50fe9a2f35fbfa928e0f3daf9bf124df6665e
pnowak2 <p.nowak2@gmail.com>
2025-12-11 14:08:44 +0100

    done

--- electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/4-advanced-io/4.3-i2c/i2c-example.py
+++ electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/4-advanced-io/4.3-i2c/i2c-example.py
@@ -1,1 +1,37 @@
 from machine import Pin, I2C
+from lib.pico_i2c_lcd import I2cLcd
+from time import sleep
+
+i2c = I2C(0, scl=Pin(1), sda=Pin(0))
+
+I2C_ADDR = i2c.scan()[0]
+
+print(f"Found lcd at {I2C_ADDR}")
+
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16 
+
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+lcd.putstr("It's working")
+sleep(2)
+
+try:
+  while True:
+    lcd.show_cursor()
+    lcd.clear()
+
+    for idx, letter in enumerate("Hello"):
+      lcd.move_to(idx, 0)
+      lcd.putstr(letter)
+      sleep(0.5)
+
+    lcd.move_to(0, 1)
+    lcd.putstr("World!")
+    sleep(2)
+
+except KeyboardInterrupt:
+  lcd.backlight_off()
+  sleep(1)
+  lcd.display_off()
+  sleep(1)
