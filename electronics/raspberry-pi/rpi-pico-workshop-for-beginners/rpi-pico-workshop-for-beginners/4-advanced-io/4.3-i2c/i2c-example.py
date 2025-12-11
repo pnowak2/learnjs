d55d01fe5056bf40fd48c281866c710fdd37977a
pnowak2 <p.nowak2@gmail.com>
2025-12-11 14:15:15 +0100

    done

--- electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/4-advanced-io/4.3-i2c/i2c-example.py
+++ electronics/raspberry-pi/rpi-pico-workshop-for-beginners/rpi-pico-workshop-for-beginners/4-advanced-io/4.3-i2c/i2c-example.py
@@ -1,9 +1,10 @@
 from machine import Pin, I2C
-from lib.pico_i2c_lcd import I2cLcd
+from lib.lcd.pico_i2c_lcd import I2cLcd
 from time import sleep
 
 i2c = I2C(0, scl=Pin(1), sda=Pin(0))
 
+# 0x29 for current model i got, can be different, then scan and check
 I2C_ADDR = i2c.scan()[0]
 
 print(f"Found lcd at {I2C_ADDR}")
@@ -13,7 +14,7 @@ I2C_NUM_COLS = 16
 
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-lcd.putstr("It's working")
+lcd.putstr("Et funktioniert!")
 sleep(2)
 
 try:
@@ -21,13 +22,13 @@ try:
     lcd.show_cursor()
     lcd.clear()
 
-    for idx, letter in enumerate("Hello"):
+    for idx, letter in enumerate("Moien"):
       lcd.move_to(idx, 0)
       lcd.putstr(letter)
       sleep(0.5)
 
     lcd.move_to(0, 1)
-    lcd.putstr("World!")
+    lcd.putstr("Luxembourg")
     sleep(2)
 
 except KeyboardInterrupt:
