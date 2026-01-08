8ced6b5a773114a17d11e0dbd5081c51e4d3a1ed
pnowak2 <p.nowak2@gmail.com>
2026-01-08 14:42:31 +0100

    done

---
+++ lang/javascript/javascript.info/2025/tests/3.data-types.test.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import { add } from '../src/2.fundamentals/add';
+
+describe('Data types', () => {
+  describe('Methods of primitives', () => {
+    it('should', () => {
+      
+    })
+  });
+});
